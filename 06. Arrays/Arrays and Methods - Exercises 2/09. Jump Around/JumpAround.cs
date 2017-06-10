130ee2a66c90c61c2d3e794512708e3d8884e4b3
origami99 <origamista99@Gmail.com>
2017-06-10 15:13:46 +0300

    Finished all exercises from "06. Arrays and Methods - More Exercises"

--- 06. Arrays/Arrays and Methods - Exercises 2/09. Jump Around/JumpAround.cs
+++ 06. Arrays/Arrays and Methods - Exercises 2/09. Jump Around/JumpAround.cs
@@ -1,9 +1,36 @@
 ﻿using System;
+using System.Linq;
 
 class JumpAround
 {
     static void Main()
     {
+        int[] arr = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
 
+        int sum = arr[0];
+        int currPos = 0;
+
+        while (true)
+        {
+            if (currPos + arr[currPos] < arr.Length)
+            {
+                currPos += arr[currPos];
+                sum += arr[currPos];
+            }
+            else if (currPos - arr[currPos] >= 0)
+            {
+                currPos -= arr[currPos];
+                sum += arr[currPos];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine(sum);
     }
 }
