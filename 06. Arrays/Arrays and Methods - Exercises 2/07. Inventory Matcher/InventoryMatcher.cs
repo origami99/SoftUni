3a5bb10932d9191982aa2e7f0f16b33303d10ee3
origami99 <origamista99@Gmail.com>
2017-06-10 12:54:02 +0300

    Resolved another problem "7. Inventory Matcher"

--- 06. Arrays/Arrays and Methods - Exercises 2/07. Inventory Matcher/InventoryMatcher.cs
+++ 06. Arrays/Arrays and Methods - Exercises 2/07. Inventory Matcher/InventoryMatcher.cs
@@ -1,9 +1,38 @@
 ﻿using System;
+using System.Linq;
 
 class InventoryMatcher
 {
     static void Main()
     {
+        string[] names = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+        long[] quantities = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse)
+            .ToArray();
+        decimal[] prices = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(decimal.Parse)
+            .ToArray();
 
+        while (true)
+        {
+            string product = Console.ReadLine();
+
+            if (product == "done")
+            {
+                break;
+            }
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (product == names[i])
+                {
+                    Console.WriteLine($"{names[i]} costs: {prices[i]}; Available quantity: {quantities[i]}");
+                }
+            }
+        }
     }
 }
