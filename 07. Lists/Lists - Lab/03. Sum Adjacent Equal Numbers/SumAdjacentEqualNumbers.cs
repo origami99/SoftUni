605f11263c1c2c69134c42f05c1c521fb8d09b19
origami99 <origamista99@Gmail.com>
2017-06-12 21:11:49 +0300

    Solved "03. Sum Adjacent Equal Numbers" from "Lists - Lab"

--- 07. Lists/Lists - Lab/03. Sum Adjacent Equal Numbers/SumAdjacentEqualNumbers.cs
+++ 07. Lists/Lists - Lab/03. Sum Adjacent Equal Numbers/SumAdjacentEqualNumbers.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 class SumAdjacentEqualNumbers
 {
     static void Main()
     {
+        List<double> nums = Console.ReadLine()
+            .Split(' ')
+            .Select(double.Parse)
+            .ToList();
 
+        bool hasEqualNeighbors = false;
+        do
+        {
+            hasEqualNeighbors = false;
+            double? lastNum = null;
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == lastNum)
+                {
+                    hasEqualNeighbors = true;
+
+                    nums.RemoveAt(i - 1);
+                    i--;
+                    nums[i] += nums[i];
+
+                    break;
+                }
+
+                lastNum = nums[i];
+            }
+        }
+        while (hasEqualNeighbors);
+
+        Console.WriteLine(string.Join(" ", nums));
     }
 }
