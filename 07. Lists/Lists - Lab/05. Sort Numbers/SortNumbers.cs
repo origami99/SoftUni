966854ae141d300a5c95f837e1e761dc3f682ce6
origami99 <origamista99@Gmail.com>
2017-06-12 23:41:58 +0300

    Solved "05. Sort Numbers" from "Lists - Lab"

--- 07. Lists/Lists - Lab/05. Sort Numbers/SortNumbers.cs
+++ 07. Lists/Lists - Lab/05. Sort Numbers/SortNumbers.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 class SortNumbers
 {
     static void Main()
     {
+        List<double> nums = Console.ReadLine()
+            .Split(' ')
+            .Select(double.Parse)
+            .ToList();
 
+        nums.Sort();
+
+        Console.WriteLine(string.Join(" <= ", nums));
     }
 }
