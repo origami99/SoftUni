241b043ba6797334c70ce6c5c2e176cebfc07af1
origami99 <origamista99@Gmail.com>
2017-06-12 16:36:27 +0300

    Solved "01. Remove Negatives and Reverse" from "Lists - Lab"

--- 07. Lists/Lists - Lab/01. Remove Negatives and Reverse/RemoveNegativesAndReverse.cs
+++ 07. Lists/Lists - Lab/01. Remove Negatives and Reverse/RemoveNegativesAndReverse.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 class RemoveNegativesAndReverse
 {
     static void Main()
     {
+        int[] nums = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
 
+        List<int> positiveList = new List<int>();
+
+        foreach (var n in nums)
+        {
+            if (n >= 0)
+            {
+                positiveList.Add(n);
+            }
+        }
+
+        positiveList.Reverse();
+
+        if (positiveList.Count != 0)
+        {
+            Console.WriteLine(string.Join(" ", positiveList));
+        }
+        else
+        {
+            Console.WriteLine("empty");
+        }
     }
 }
