52858adab6f20dce99d45887fd11f3d2f72e44a6
origami99 <origamista99@Gmail.com>
2017-06-12 17:14:21 +0300

    Solved "02. Append Lists" from "Lists - Lab"

--- 07. Lists/Lists - Lab/02. Append Lists/AppendLists.cs
+++ 07. Lists/Lists - Lab/02. Append Lists/AppendLists.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 class AppendLists
 {
     static void Main()
     {
+        List<string> sequences = Console.ReadLine()
+            .Split('|')
+            .ToList();
 
+        List<int> nums = new List<int>();
+
+        for (int i = sequences.Count - 1; i >= 0; i--)
+        {
+            List<int> temp = sequences[i]
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            foreach (var num in temp)
+            {
+                nums.Add(num);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", nums));
     }
 }
