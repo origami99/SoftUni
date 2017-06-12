c26c87c862bb7d7443ae281df3b7477addf775ac
origami99 <origamista99@Gmail.com>
2017-06-13 00:46:15 +0300

    Solved "05. Sort Numbers" from "Lists - Lab"

--- 07. Lists/Lists - Lab/06. Square Numbers/SquareNumbers.cs
+++ 07. Lists/Lists - Lab/06. Square Numbers/SquareNumbers.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 class SquareNumbers
 {
     static void Main()
     {
+        List<int> nums = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
 
+        List<int> exactSqrts = new List<int>();
+
+        for (int i = 0; i < nums.Count; i++)
+        {
+            if (Math.Sqrt(nums[i]) == (int)Math.Sqrt(nums[i]))
+            {
+                exactSqrts.Add(nums[i]);
+            }
+        }
+
+        exactSqrts.Sort();
+        exactSqrts.Reverse();
+
+        Console.WriteLine(string.Join(" ", exactSqrts));
     }
 }
