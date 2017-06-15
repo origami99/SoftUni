598d5f545b6812c5814c5c79f94e8c46b5625152
origami99 <origamista99@Gmail.com>
2017-06-15 23:30:20 +0300

    Solved most of the problems in "List - Exercises"

--- 07. Lists/Lists - Exercises/01. Max Sequence of Equal Elements/MaxSequenceOfEqualElements.cs
+++ 07. Lists/Lists - Exercises/01. Max Sequence of Equal Elements/MaxSequenceOfEqualElements.cs
@@ -1,10 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 
 class MaxSequenceOfEqualElements
 {
     static void Main()
     {
+        int[] nums = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
 
+        int currLength = 0;
+        int? currStart = null;
+        int? prevNum = null;
+        int? bestStart = null;
+        int bestLength = int.MinValue;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == prevNum)
+            {
+                if (currLength == 0)
+                {
+                    currStart = i;
+                }
+
+                currLength++;
+
+                if (currLength > bestLength)
+                {
+                    bestLength = currLength;
+                    bestStart = currStart;
+                }
+            }
+            else
+            {
+                currLength = 0;
+            }
+
+            prevNum = nums[i];
+        }
+
+        for (int i = 0; i <= bestLength; i++)
+        {
+            Console.Write(nums[(int)bestStart] + " ");
+        }
+        Console.WriteLine();
     }
 }
