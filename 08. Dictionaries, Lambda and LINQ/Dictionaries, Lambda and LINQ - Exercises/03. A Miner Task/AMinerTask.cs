eb7044b39a1ab74616929da78e21fedd156f5c25
origami99 <origamista99@Gmail.com>
2017-06-16 21:44:18 +0300

    Solved "03. A Miner Task"

--- 08. Dictionaries, Lambda and LINQ/Dictionaries, Lambda and LINQ - Exercises/03. A Miner Task/AMinerTask.cs
+++ 08. Dictionaries, Lambda and LINQ/Dictionaries, Lambda and LINQ - Exercises/03. A Miner Task/AMinerTask.cs
@@ -1,9 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class AMinerTask
 {
     static void Main()
     {
+        string resource = Console.ReadLine();
 
+        Dictionary<string, long> inventar = new Dictionary<string, long>();
+
+        while (resource != "stop")
+        {
+            long quantity = long.Parse(Console.ReadLine());
+
+            if (inventar.ContainsKey(resource))
+            {
+                inventar[resource] += quantity;
+            }
+            else
+            {
+                inventar[resource] = quantity;
+            }
+
+            resource = Console.ReadLine();
+        }
+
+        foreach (var item in inventar)
+        {
+            Console.WriteLine("{0} -> {1}",
+                item.Key, item.Value);
+        }
     }
 }
