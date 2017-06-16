23b4374d3464e9f6cf3d84d85c1d1af86824f155
origami99 <origamista99@Gmail.com>
2017-06-16 19:21:02 +0300

    Solved "01. Phonebook and "02. Phonebook Upgrade"

--- 08. Dictionaries, Lambda and LINQ/Dictionaries, Lambda and LINQ - Exercises/01. Phonebook/Phonebook.cs
+++ 08. Dictionaries, Lambda and LINQ/Dictionaries, Lambda and LINQ - Exercises/01. Phonebook/Phonebook.cs
@@ -1,9 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Phonebook
 {
     static void Main()
     {
+        string[] entry = Console.ReadLine()
+            .Split(' ')
+            .ToArray();
 
+        Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+        while (entry[0] != "END")
+        {
+            if (entry[0] == "A")
+            {
+                phonebook[entry[1]] = entry[2];
+            }
+            else if (entry[0] == "S")
+            {
+                if (phonebook.ContainsKey(entry[1]))
+                {
+                    Console.WriteLine($"{entry[1]} -> {phonebook[entry[1]]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Contact {entry[1]} does not exist.");
+                }
+            }
+
+            entry = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+        }
     }
 }
--- 08. Dictionaries, Lambda and LINQ/Dictionaries, Lambda and LINQ - Exercises/02. Phonebook Upgrade/PhonebookUpgrade.cs
+++ 08. Dictionaries, Lambda and LINQ/Dictionaries, Lambda and LINQ - Exercises/02. Phonebook Upgrade/PhonebookUpgrade.cs
@@ -1,9 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class PhonebookUpgrade
 {
     static void Main()
     {
+        string[] entry = Console.ReadLine()
+            .Split(' ')
+            .ToArray();
 
+        SortedDictionary<string, string> phonebook = new SortedDictionary<string, string>();
+
+        while (entry[0] != "END")
+        {
+
+            switch (entry[0])
+            {
+                case "A":
+                    phonebook[entry[1]] = entry[2];
+                    break;
+                case "S":
+                    if (phonebook.ContainsKey(entry[1]))
+                    {
+                        Console.WriteLine($"{entry[1]} -> {phonebook[entry[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {entry[1]} does not exist.");
+                    }
+                    break;
+                case "ListAll":
+                    foreach (var item in phonebook)
+                    {
+                        Console.WriteLine("{0} -> {1}",
+                            item.Key, item.Value);
+                    }
+                    break;
+            }
+
+            entry = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+        }
     }
 }
