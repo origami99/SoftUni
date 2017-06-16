5b2529ce9090053e46f9a7c602f62cbaf5180df1
origami99 <origamista99@Gmail.com>
2017-06-16 15:04:18 +0300

    Finished all problems at "Lists - Exercises"

--- 07. Lists/Lists - Exercises/04. Longest Increasing Subsequence/LongestIncreasingSubsequence.cs
+++ 07. Lists/Lists - Exercises/04. Longest Increasing Subsequence/LongestIncreasingSubsequence.cs
@@ -6,6 +6,46 @@ class LongestIncreasingSubsequence
 {
     static void Main()
     {
+        int[] nums = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
 
+        int[] len = new int[nums.Length];
+        int[] prev = new int[nums.Length];
+        int bestLength = 0;
+        int lastIndex = -1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            len[i] = 1;
+            prev[i] = -1;
+
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i] && len[j] >= len[i])
+                {
+                    len[i] = 1 + len[j];
+                    prev[i] = j;
+                }
+            }
+
+            if (len[i] > bestLength)
+            {
+                bestLength = len[i];
+                lastIndex = i;
+            }
+        }
+
+        List<int> longestSeq = new List<int>();
+        for (int i = 0; i < bestLength; i++)
+        {
+            longestSeq.Add(nums[lastIndex]);
+            lastIndex = prev[lastIndex];
+        }
+
+        longestSeq.Reverse();
+
+        Console.WriteLine(string.Join(" ", longestSeq));
     }
 }
