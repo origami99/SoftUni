e948d656ff238a0fa4c146125baf0726c35cf4cf
origami99 <origamista99@Gmail.com>
2017-06-22 13:19:22 +0300

    Solved "1. Sort Times" and "2. Odd Filter"

--- 08. Dictionaries, Lambda and LINQ/Dictionaries and Lists - Exercises 2/2. Odd Filter/OddFilter.cs
+++ 08. Dictionaries, Lambda and LINQ/Dictionaries and Lists - Exercises 2/2. Odd Filter/OddFilter.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Linq;
 
-class ImmuneSystem
+class OddFilter
 {
     static void Main()
     {
+        int[] nums = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .Where(x => x % 2 == 0)
+            .ToArray();
 
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > nums.Average())
+            {
+                nums[i]++;
+            }
+            else
+            {
+                nums[i]--;
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", nums));
     }
 }
--- 08. Dictionaries, Lambda and LINQ/Dictionaries and Lists - Exercises 2/3. Immune System/ImmuneSystem.cs
+++ 08. Dictionaries, Lambda and LINQ/Dictionaries and Lists - Exercises 2/3. Immune System/ImmuneSystem.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-class Program
+class ImmuneSystem
 {
     static void Main()
     {
