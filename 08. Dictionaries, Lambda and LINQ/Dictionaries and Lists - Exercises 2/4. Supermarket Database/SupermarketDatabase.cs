71ca631e08d46950983fb75f95b3594a50652e5c
origami99 <origamista99@Gmail.com>
2017-06-22 21:00:14 +0300

    Solved "4. Supermarket Database"

--- 08. Dictionaries, Lambda and LINQ/Dictionaries and Lists - Exercises 2/4. Supermarket Database/SupermarketDatabase.cs
+++ 08. Dictionaries, Lambda and LINQ/Dictionaries and Lists - Exercises 2/4. Supermarket Database/SupermarketDatabase.cs
@@ -1,9 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class SupermarketDatabase
 {
     static void Main()
     {
+        Dictionary<string, double[]> products = new Dictionary<string, double[]>();
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "stocked")
+            {
+                break;
+            }
 
+            string name = input.Split(' ').First();
+            double price = double.Parse(input.Split(' ').Skip(1).First());
+            int quantity = int.Parse(input.Split(' ').Last());
+
+            if (products.ContainsKey(name))
+            {
+                products[name][0] = price;
+                products[name][1] += quantity;
+            }
+            else
+            {
+                products[name] = new double[] { price, quantity };
+            }
+        }
+
+        double grandPrice = 0;
+        foreach (var item in products)
+        {
+            double totalPrice = item.Value[0] * item.Value[1];
+            grandPrice += totalPrice;
+
+            Console.WriteLine($"{item.Key:F2}: ${item.Value[0]:F2} * {item.Value[1]} = ${totalPrice:F2}");
+        }
+
+        Console.WriteLine("------------------------------");
+
+        Console.WriteLine($"Grand Total: ${grandPrice:F2}");
     }
 }
