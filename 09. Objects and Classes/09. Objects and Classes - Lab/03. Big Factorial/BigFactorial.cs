0396389333ca18cd4242bc562ba4f4ac863e6750
origami99 <origamista99@Gmail.com>
2017-06-24 18:35:00 +0300

    Solved "03. Big Factorial"

--- 09. Objects and Classes/09. Objects and Classes - Lab/03. Big Factorial/BigFactorial.cs
+++ 09. Objects and Classes/09. Objects and Classes - Lab/03. Big Factorial/BigFactorial.cs
@@ -6,8 +6,8 @@ class BigFactorial
     static void Main()
     {
         int n = int.Parse(Console.ReadLine());
-
         BigInteger factorial = 1;
+
         for (int i = 1; i <= n; i++)
         {
             factorial *= i;
