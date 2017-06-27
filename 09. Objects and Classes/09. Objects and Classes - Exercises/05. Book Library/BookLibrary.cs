a20ab16791a53bfdf72d46dc4813907d40d26405
origami99 <origamista99@Gmail.com>
2017-06-27 19:28:02 +0300

    Solved "05. Book Library" and "06. Book Library Modification"

--- 09. Objects and Classes/09. Objects and Classes - Exercises/05. Book Library/BookLibrary.cs
+++ 09. Objects and Classes/09. Objects and Classes - Exercises/05. Book Library/BookLibrary.cs
@@ -1,9 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
-class BookLibrary
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Publisher { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string ISBN { get; set; }
+    public double Price { get; set; }
+}
+
+class Library
+{
+    public string Name { get; set; }
+    public List<Book> Books { get; set; }
+}
+
+class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        Library library = new Library();
+        library.Books = new List<Book>();
+        Dictionary<string, double> authors = new Dictionary<string, double>();
+        for (int i = 0; i < n; i++)
+        {
+            Book book = ReadBook();
+            library.Books.Add(book);
+
+            if (authors.ContainsKey(book.Author))
+            {
+                authors[book.Author] += book.Price;
+            }
+            else
+            {
+                authors[book.Author] = book.Price;
+            }
+        }
+
+        authors = authors
+            .OrderByDescending(b => b.Value)
+            .ThenBy(b => b.Key)
+            .ToDictionary(a => a.Key, p => p.Value);
+
+        foreach (var a in authors)
+        {
+            Console.WriteLine($"{a.Key} -> {a.Value:F2}");
+        }
+    }
+
+    private static Book ReadBook()
+    {
+        string[] input = Console.ReadLine()
+            .Split(' ')
+            .ToArray();
+
+        Book book = new Book();
+
+        book.Title = input[0];
+        book.Author = input[1];
+        book.Publisher = input[2];
+        book.ReleaseDate = DateTime.ParseExact(
+            input[3], 
+            "dd.MM.yyyy", 
+            CultureInfo.InvariantCulture);
+        book.ISBN = input[4];
+        book.Price = double.Parse(input[5]);
+
+        return book;
     }
 }
--- 09. Objects and Classes/09. Objects and Classes - Exercises/06. Book Library Modification/BookLibraryModification.cs
+++ 09. Objects and Classes/09. Objects and Classes - Exercises/06. Book Library Modification/BookLibraryModification.cs
@@ -1,9 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
-class BookLibraryModification
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Publisher { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string ISBN { get; set; }
+    public double Price { get; set; }
+}
+
+class Library
+{
+    public string Name { get; set; }
+    public List<Book> Books { get; set; }
+}
+
+class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        Library library = new Library();
+        library.Books = new List<Book>();
+        Dictionary<string, DateTime> titles = new Dictionary<string, DateTime>();
+        for (int i = 0; i < n; i++)
+        {
+            Book book = ReadBook();
+            library.Books.Add(book);
+            titles[book.Title] = book.ReleaseDate;
+        }
+
+        DateTime givenDate = DateTime.ParseExact(
+            Console.ReadLine(),
+            "dd.MM.yyyy",
+            CultureInfo.InvariantCulture);
+
+        titles = titles
+            .Where(b => b.Value > givenDate)
+            .OrderBy(b => b.Value)
+            .ThenBy(b => b.Key)
+            .ToDictionary(a => a.Key, p => p.Value);
+
+        foreach (var a in titles)
+        {
+            Console.WriteLine($"{a.Key} -> {string.Format("{0:dd.MM.yyyy}" ,a.Value)}");
+        }
+    }
+
+    private static Book ReadBook()
+    {
+        string[] input = Console.ReadLine()
+            .Split(' ')
+            .ToArray();
+
+        Book book = new Book();
+
+        book.Title = input[0];
+        book.Author = input[1];
+        book.Publisher = input[2];
+        book.ReleaseDate = DateTime.ParseExact(
+            input[3],
+            "dd.MM.yyyy",
+            CultureInfo.InvariantCulture);
+        book.ISBN = input[4];
+        book.Price = double.Parse(input[5]);
+
+        return book;
     }
 }
