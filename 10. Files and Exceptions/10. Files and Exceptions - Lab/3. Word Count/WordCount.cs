f89fc974748906b8fa7ba90ac67a3d79f7b86d88
origami99 <origamista99@Gmail.com>
2017-06-28 23:49:05 +0300

    Solved "3. Word Count"

--- 10. Files and Exceptions/10. Files and Exceptions - Lab/3. Word Count/WordCount.cs
+++ 10. Files and Exceptions/10. Files and Exceptions - Lab/3. Word Count/WordCount.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 class WordCount
 {
     static void Main()
     {
+        string[] words = File.ReadAllText("words.txt")
+            .ToLower()
+            .Split();
+        string[] text = File.ReadAllText("input.txt")
+            .ToLower()
+            .Split(new char[] { '\n', '\r', ' ', '.', ',', '!', '?', '-' },
+            StringSplitOptions.RemoveEmptyEntries);
 
+        Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+        foreach (string w in words)
+        {
+            wordCounts[w] = 0;
+        }
+
+        foreach (string word in text)
+        {
+            if (wordCounts.ContainsKey(word))
+            {
+                wordCounts[word]++;
+            }
+        }
+
+        wordCounts = wordCounts
+            .OrderByDescending(x => x.Value)
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        foreach (var word in wordCounts)
+        {
+            Console.WriteLine($"{word.Key} -> {word.Value}");
+        }
     }
 }
