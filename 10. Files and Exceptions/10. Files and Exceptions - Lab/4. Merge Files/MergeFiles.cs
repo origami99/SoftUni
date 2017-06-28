487208f6a01cb2a097c272fc82852a55ada1e2a1
origami99 <origamista99@Gmail.com>
2017-06-29 00:30:57 +0300

    Solved "4. Merge Files"

--- 10. Files and Exceptions/10. Files and Exceptions - Lab/4. Merge Files/MergeFiles.cs
+++ 10. Files and Exceptions/10. Files and Exceptions - Lab/4. Merge Files/MergeFiles.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 class MergeFiles
 {
     static void Main()
     {
+        int[] file1 = File.ReadAllLines("input1.txt")
+            .Select(int.Parse)
+            .ToArray();
+        int[] file2 = File.ReadAllLines("input2.txt")
+            .Select(int.Parse)
+            .ToArray();
 
+        string[] concated = file1.Concat(file2)
+            .OrderBy(number => number)
+            .Select(x => Convert.ToString(x))
+            .ToArray();
+
+        File.Create("output.txt").Close();
+
+        File.AppendAllLines("output.txt", concated);
     }
 }
