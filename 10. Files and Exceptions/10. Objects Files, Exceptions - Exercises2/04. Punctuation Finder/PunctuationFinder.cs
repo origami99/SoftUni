4558c0f26a84f8f7152816e2a42687e35332c63c
origami99 <origamista99@Gmail.com>
2017-06-30 19:18:13 +0300

    Solved "04. Punctuation Finder" and "05. Write to File"

--- 10. Files and Exceptions/10. Objects Files, Exceptions - Exercises2/04. Punctuation Finder/PunctuationFinder.cs
+++ 10. Files and Exceptions/10. Objects Files, Exceptions - Exercises2/04. Punctuation Finder/PunctuationFinder.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 class PunctuationFinder
 {
     static void Main()
     {
+        string text = File.ReadAllText("sample_text.txt");
+        var punctuacionMarks = Regex.Matches(text , "[.,!?:]");
 
+        int counter = 0;
+        foreach (var mark in punctuacionMarks)
+        {
+            counter++;
+
+            Console.Write(mark);
+            if (counter != punctuacionMarks.Count)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine();
     }
 }
--- 10. Files and Exceptions/10. Objects Files, Exceptions - Exercises2/05. Write to File/WriteToFile.cs
+++ 10. Files and Exceptions/10. Objects Files, Exceptions - Exercises2/05. Write to File/WriteToFile.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 class WriteToFile
 {
     static void Main()
     {
+        string text = File.ReadAllText("sample_text.txt");
+        string textWithoutMarks = Regex.Replace(text, "[.,!?:]", "");
 
+        File.Create("output.txt").Close();
+        File.WriteAllText("output.txt", textWithoutMarks);
     }
 }
