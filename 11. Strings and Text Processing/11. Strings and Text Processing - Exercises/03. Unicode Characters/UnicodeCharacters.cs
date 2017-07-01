3be8cef8c60607952ff1f8235d85b65e6ed17770
origami99 <origamista99@Gmail.com>
2017-07-01 14:38:32 +0300

    Solved "03. Unicode Characters"

--- 11. Strings and Text Processing/11. Strings and Text Processing - Exercises/03. Unicode Characters/UnicodeCharacters.cs
+++ 11. Strings and Text Processing/11. Strings and Text Processing - Exercises/03. Unicode Characters/UnicodeCharacters.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 class UnicodeCharacters
 {
     static void Main()
     {
+        char[] text = Console.ReadLine().ToCharArray();
 
+        string finalHex = string.Empty;
+        foreach (var symb in text)
+        {
+            string hex = Convert.ToString(symb, 16);
+            string zeros = new string('0', 4 - hex.Length);
+            string hexChar = $@"\u{zeros}{hex:D4}";
+            finalHex += hexChar;
+        }
+
+        Console.WriteLine(finalHex);
     }
 }
