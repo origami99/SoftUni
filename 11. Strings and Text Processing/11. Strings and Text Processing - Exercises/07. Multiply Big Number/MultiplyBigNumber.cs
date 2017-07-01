602a49e83555f9df50674e5db5212b5d4b8e60bc
origami99 <origamista99@Gmail.com>
2017-07-01 20:45:54 +0300

    Solved "07. Multiply Big Number"

--- 11. Strings and Text Processing/11. Strings and Text Processing - Exercises/07. Multiply Big Number/MultiplyBigNumber.cs
+++ 11. Strings and Text Processing/11. Strings and Text Processing - Exercises/07. Multiply Big Number/MultiplyBigNumber.cs
@@ -1,9 +1,41 @@
 ﻿using System;
+using System.Linq;
 
 class MultiplyBigNumber
 {
     static void Main()
     {
+        string bigNum = Console.ReadLine().TrimStart('0').Trim();
+        int smallNum = int.Parse(Console.ReadLine());
 
+        if (smallNum == 0 || bigNum == string.Empty)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+
+        bigNum = new string(bigNum.Reverse().ToArray());
+
+        string product = string.Empty;
+        int leftDigit = 0;
+        for (int i = 0; i < bigNum.Length; i++)
+        {
+            int digit = bigNum[i] - '0';
+            string multiplyDigits = (smallNum * digit + leftDigit).ToString();
+
+            leftDigit = 0;
+            if (multiplyDigits.Length == 1 || i == bigNum.Length - 1)
+            {
+                product = multiplyDigits + product;
+            }
+            else
+            {
+                leftDigit = multiplyDigits[0] - '0';
+                int rightDigit = multiplyDigits[1] - '0';
+                product = rightDigit + product;
+            }
+        }
+
+        Console.WriteLine(product);
     }
 }
