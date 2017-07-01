71286190638e6c8c931ef26fe495beeb51a96a12
origami99 <origamista99@Gmail.com>
2017-07-01 03:10:23 +0300

    Solved "01. Convert from Base 10 to Base N"

--- 11. Strings and Text Processing/11. Strings and Text Processing - Exercises/01. Convert from Base 10 to Base N/ConvertFromBase10ToBaseN.cs
+++ 11. Strings and Text Processing/11. Strings and Text Processing - Exercises/01. Convert from Base 10 to Base N/ConvertFromBase10ToBaseN.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Linq;
+using System.Numerics;
 
 class ConvertFromBase10ToBaseN
 {
     static void Main()
     {
+        BigInteger[] input = Console.ReadLine()
+            .Split(' ')
+            .Select(x => BigInteger.Parse(x))
+            .ToArray();
+        BigInteger baseN = input.First();
+        BigInteger base10Num = input.Last();
 
+        string baseNNum = string.Empty;
+        while (base10Num > 0)
+        {
+            baseNNum = $"{base10Num % baseN}{baseNNum}";
+            base10Num /= baseN;
+        }
+
+        Console.WriteLine(baseNNum);
     }
 }
