44211bf8875dbbe00e860e9ed4ef7a44454c420f
origami99 <origamista99@Gmail.com>
2017-07-02 19:16:13 +0300

    Solved "03. Match Hexadecimal Numbers"

--- 12. Regex/12. RegEx - Lab/03. Match Hexadecimal Numbers/MatchHexadecimalNumbers.cs
+++ 12. Regex/12. RegEx - Lab/03. Match Hexadecimal Numbers/MatchHexadecimalNumbers.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 class MatchHexadecimalNumbers
 {
     static void Main()
     {
+        string input = Console.ReadLine();
+        string pattern = @"\b(?:0x)?[0-9A-F]+\b";
 
+        MatchCollection hexNums = Regex
+            .Matches(input, pattern);
+
+        foreach (var hex in hexNums)
+        {
+            Console.Write($"{hex} ");
+        }
+        Console.WriteLine();
     }
 }
