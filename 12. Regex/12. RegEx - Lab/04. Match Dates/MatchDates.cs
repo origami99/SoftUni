d03a9d34addbdc1e7918fa7130a6d7091251ee46
origami99 <origamista99@Gmail.com>
2017-07-02 20:50:26 +0300

    Solved "04. Match Dates"

--- 12. Regex/12. RegEx - Lab/04. Match Dates/MatchDates.cs
+++ 12. Regex/12. RegEx - Lab/04. Match Dates/MatchDates.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 class MatchDates
 {
     static void Main()
     {
+        string input = Console.ReadLine();
+        string pattern = 
+            @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
 
+        MatchCollection validDates = Regex.Matches(input, pattern);
+
+        foreach (Match date in validDates)
+        {
+            string day = date.Groups["day"].Value;
+            string month = date.Groups["month"].Value;
+            string year = date.Groups["year"].Value;
+
+            Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+        }
     }
 }
