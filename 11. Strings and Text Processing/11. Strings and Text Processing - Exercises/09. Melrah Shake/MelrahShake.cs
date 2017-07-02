a7925fe50e1df876cd886d3dcb3a11ced0065af2
origami99 <origamista99@Gmail.com>
2017-07-02 04:47:59 +0300

    Solved "09. Melrah Shake"

--- 11. Strings and Text Processing/11. Strings and Text Processing - Exercises/09. Melrah Shake/MelrahShake.cs
+++ 11. Strings and Text Processing/11. Strings and Text Processing - Exercises/09. Melrah Shake/MelrahShake.cs
@@ -4,6 +4,36 @@ class MelrahShake
 {
     static void Main()
     {
+        string text = Console.ReadLine();
+        string pattern = Console.ReadLine();
 
+        while (true)
+        {
+            int firstIndex = text.IndexOf(pattern);
+            int lastIndex = text.LastIndexOf(pattern);
+
+            if (firstIndex >= 0 && lastIndex >= 0 && pattern != "")
+            {
+                Console.WriteLine("Shaked it.");
+
+                firstIndex = text.IndexOf(pattern);
+                text = text.Remove(firstIndex, pattern.Length);
+
+                lastIndex = text.LastIndexOf(pattern);
+                text = text.Remove(lastIndex, pattern.Length);
+
+                if (pattern.Length > 0)
+                {
+                    pattern = pattern.Remove(pattern.Length / 2, 1);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No shake.");
+                break;
+            }
+        }
+
+        Console.WriteLine(text);
     }
 }
