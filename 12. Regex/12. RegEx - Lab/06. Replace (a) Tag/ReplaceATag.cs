2138d686326d659a0d9fd28eaff87c8799a4b8e0
origami99 <origamista99@Gmail.com>
2017-07-03 17:45:56 +0300

    Solved "06. Replace (a) Tag"

--- 12. Regex/12. RegEx - Lab/06. Replace (a) Tag/ReplaceATag.cs
+++ 12. Regex/12. RegEx - Lab/06. Replace (a) Tag/ReplaceATag.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 class ReplaceATag
 {
     static void Main()
     {
+        string tags = Console.ReadLine();
+        while (tags != "end")
+        {
+            string pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+            string replacement = @"[URL href=$1]$2[/URL]";
+            string replaced = Regex.Replace(
+                tags, pattern, replacement);
+            Console.WriteLine(replaced);
 
+            tags = Console.ReadLine();
+        }
     }
 }
