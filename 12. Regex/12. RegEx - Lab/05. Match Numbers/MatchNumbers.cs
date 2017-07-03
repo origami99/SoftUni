9dd2e4526f0faee2cf6e198788a118a8ce33485e
origami99 <origamista99@Gmail.com>
2017-07-03 03:54:58 +0300

    Solved "05. Match Numbers"

--- 12. Regex/12. RegEx - Lab/05. Match Numbers/MatchNumbers.cs
+++ 12. Regex/12. RegEx - Lab/05. Match Numbers/MatchNumbers.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 class MatchNumbers
 {
     static void Main()
     {
+        string input = Console.ReadLine();
+        string pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
 
+        MatchCollection validNums = Regex.Matches(input, pattern);
+
+        foreach (Match num in validNums)
+        {
+            Console.Write($"{num} ");
+        }
+        Console.WriteLine();
     }
 }
