228f4d72264a62bf752af93b696abd4f06c8b4b5
origami99 <origamista99@Gmail.com>
2017-07-04 00:38:03 +0300

    Solved "01. Extract Emails"

--- 12. Regex/12. RegEx - Exercises/01. Extract Emails/ExtractEmails.cs
+++ 12. Regex/12. RegEx - Exercises/01. Extract Emails/ExtractEmails.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 class ExtractEmails
 {
     static void Main()
     {
+        string text = Console.ReadLine();
+        string pattern = 
+            @"(?<=\s|^)[a-zA-Z0-9]+[\.\-\w]*[a-zA-Z0-9]+@[a-zA-Z0-9]+[\.\-\w]*\.[a-zA-Z0-9]+";
 
+        MatchCollection emails = Regex.Matches(text, pattern);
+
+        foreach (Match email in emails)
+        {
+            Console.WriteLine(email);
+        }
     }
 }
