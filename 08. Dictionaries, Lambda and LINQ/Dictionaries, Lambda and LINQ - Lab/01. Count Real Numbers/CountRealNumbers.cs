1113f621b08dbbc26148a06d62886319d9065eee
origami99 <origamista99@Gmail.com>
2017-07-13 01:29:03 +0300

    Solved some tasks from Dictionaries - Lab

--- 08. Dictionaries, Lambda and LINQ/Dictionaries, Lambda and LINQ - Lab/01. Count Real Numbers/CountRealNumbers.cs
+++ 08. Dictionaries, Lambda and LINQ/Dictionaries, Lambda and LINQ - Lab/01. Count Real Numbers/CountRealNumbers.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class CountRealNumbers
 {
     static void Main()
     {
+        double[] nums = Console.ReadLine()
+            .Split(' ')
+            .Select(x => double.Parse(x))
+            .ToArray();
 
+        SortedDictionary<double, int> globalCounter = new SortedDictionary<double, int>();
+
+        foreach (double num in nums)
+        {
+            if (globalCounter.ContainsKey(num))
+            {
+                globalCounter[num]++;
+            }
+            else
+            {
+                globalCounter[num] = 1;
+            }
+        }
+
+        foreach (var num in globalCounter)
+        {
+            Console.WriteLine($"{num.Key} -> {num.Value} times");
+        }
     }
 }
--- 08. Dictionaries, Lambda and LINQ/Dictionaries, Lambda and LINQ - Lab/02. Odd Occurrences/OddOccurrences.cs
+++ 08. Dictionaries, Lambda and LINQ/Dictionaries, Lambda and LINQ - Lab/02. Odd Occurrences/OddOccurrences.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class OddOccurrences
 {
     static void Main()
     {
+        string[] words = Console.ReadLine().Split();
 
+        Dictionary<string, int> wordCounter = new Dictionary<string, int>();
+
+        foreach (string word in words)
+        {
+            if (wordCounter.ContainsKey(word)) { wordCounter[word]++; }
+            else { wordCounter[word] = 1; }
+        }
+
+        //wordCounter = wordCounter
+        //    .Select(x => x.Key.ToLower())
+        //    .Where(x => x.Value % 2 != 0)
+        //    .ToDictionary(x => x.Key, x => x.Value);
+
+        Console.WriteLine(string.Join(", ", wordCounter.Keys));
     }
 }
