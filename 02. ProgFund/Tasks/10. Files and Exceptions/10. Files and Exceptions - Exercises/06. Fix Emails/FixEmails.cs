8a1067c7058609e23e4438179088020ada309e42
origami99 <origamista99@Gmail.com>
2017-07-13 23:18:16 +0300

    Solved "06. Fix Emails"

--- 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/06. Fix Emails/FixEmails.cs
+++ 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/06. Fix Emails/FixEmails.cs
@@ -4,6 +4,21 @@ class FixEmails
 {
     static void Main()
     {
+        while (true)
+        {
+            string name = Console.ReadLine();
+            if (name == "stop")
+            {
+                return;
+            }
+            string email = Console.ReadLine();
 
+            if (email.EndsWith("us") || email.EndsWith("uk"))
+            {
+                continue;
+            }
+
+            Console.WriteLine($"{name} -> {email}");
+        }
     }
 }
