b6cc8d1251ee1f6582f45d5c86f675d9d6979d43
origami99 <origamista99@Gmail.com>
2017-07-13 21:14:46 +0300

    Solved "03. Equal Sums"

--- 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/03. Equal Sums/EqualSums.cs
+++ 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/03. Equal Sums/EqualSums.cs
@@ -1,9 +1,39 @@
 ﻿using System;
+using System.Linq;
 
 class EqualSums
 {
     static void Main()
     {
+        int[] nums = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
 
+        if (nums.Length == 1)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+
+        bool hasEqualSums = false;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int leftSum = nums.Take(i).Sum();
+            int rightSum = nums.Skip(i + 1).Sum();
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine(i);
+                hasEqualSums = true;
+                return;
+            }
+        }
+
+        if (!hasEqualSums)
+        {
+            Console.WriteLine("no");
+        }
     }
 }
