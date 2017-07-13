91d9e542803fd3bf41fda32591c1d77fd317e5fe
origami99 <origamista99@Gmail.com>
2017-07-13 23:08:32 +0300

    Solved "05. A Miner Task"

--- 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/05. A Miner Task/AMinerTask.cs
+++ 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/05. A Miner Task/AMinerTask.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class AMinerTask
 {
     static void Main()
     {
+        Dictionary<string, long> resourses = new Dictionary<string, long>();
 
+        int counter = 0;
+        while (true)
+        {
+            string res = Console.ReadLine();
+            if (res == "stop")
+            {
+                break;
+            }
+            int amount = int.Parse(Console.ReadLine());
+
+            if (resourses.ContainsKey(res)) 
+            {
+                resourses[res] += amount;
+            }
+            else
+            {
+                resourses[res] = amount;
+            }
+
+            counter++;
+        }
+
+        foreach (KeyValuePair<string, long> ore in resourses)
+        {
+            Console.WriteLine($"{ore.Key} -> {ore.Value}");
+        }
     }
 }
