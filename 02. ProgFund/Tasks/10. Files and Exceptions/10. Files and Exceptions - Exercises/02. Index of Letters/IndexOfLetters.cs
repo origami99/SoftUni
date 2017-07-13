4d9dc2a22772dd9acc5456811465068afd635777
origami99 <origamista99@Gmail.com>
2017-07-13 19:43:04 +0300

    Solved the super easy task "02. Index of Letters"

--- 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/02. Index of Letters/IndexOfLetters.cs
+++ 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/02. Index of Letters/IndexOfLetters.cs
@@ -4,6 +4,11 @@ class IndexOfLetters
 {
     static void Main()
     {
+        string word = Console.ReadLine().ToLower();
 
+        foreach (char letter in word)
+        {
+            Console.WriteLine($"{letter} -> {letter - 97}");
+        }
     }
 }
