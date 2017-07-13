c0859fe0cd82bd83f7204861255c1839a351053c
origami99 <origamista99@Gmail.com>
2017-07-13 22:03:20 +0300

    Figured brand new shity algorith (just for fun) for "04. Max Sequence of Equal Elements"

--- 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/04. Max Sequence of Equal Elements/MaxSequenceOfEqualElements.cs
+++ 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/04. Max Sequence of Equal Elements/MaxSequenceOfEqualElements.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class MaxSequenceOfEqualElements
 {
+    /// <summary>
+    /// Do not use this algorithm in real software - it's slow.
+    /// </summary>
     static void Main()
     {
+        string[] nums = Console.ReadLine().Split();
 
+        string[] bestPart = new string[0];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 1; j <= nums.Length - i; j++)
+            {
+                string[] subArray = nums.Skip(i).Take(j).ToArray();
+
+                if (subArray.All(x => x == subArray[0])
+                    && subArray.Length > bestPart.Length)
+                {
+                    bestPart = subArray;
+                }
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", bestPart));
     }
 }
