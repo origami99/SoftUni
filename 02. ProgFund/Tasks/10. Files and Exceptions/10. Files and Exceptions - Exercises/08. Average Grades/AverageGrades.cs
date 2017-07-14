fc9b378fffc57c2602dd7a50f943f89651958542
origami99 <origamista99@Gmail.com>
2017-07-14 16:25:10 +0300

    Solved "09. Book Library" from Exercises: Files, Directories and Exceptions

--- 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/08. Average Grades/AverageGrades.cs
+++ 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/08. Average Grades/AverageGrades.cs
@@ -1,9 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Student
+{
+    public string Name { get; set; }
+    public double[] Grades { get; set; }
+
+    public double Average
+    {
+        get { return Grades.Average(); }
+    }
+}
 
 class AverageGrades
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+        
+        List<Student> students = new List<Student>();
 
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            string name = input.First();
+            double[] grades = input.Skip(1).Select(double.Parse).ToArray();
+
+            students.Add(new Student { Name = name, Grades = grades });
+        }
+
+        students = students
+            .OrderBy(x => x.Name)
+            .ThenByDescending(x => x.Average)
+            .ToList();
+
+        foreach (var stud in students)
+        {
+            if (stud.Average >= 5.00)
+            {
+                Console.WriteLine($"{stud.Name} -> {stud.Average:F2}");
+            }
+        }
     }
 }
--- 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/09. Book Library/BookLibrary.cs
+++ 02. ProgFund/Tasks/10. Files and Exceptions/10. Files and Exceptions - Exercises/09. Book Library/BookLibrary.cs
@@ -1,9 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
-class BookLibrary
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Publisher { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string ISBN { get; set; }
+    public double Price { get; set; }
+}
+
+class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        Dictionary<string, double> library = new Dictionary<string, double>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Book book = new Book
+            {
+                Title = input[0],
+                Author = input[1],
+                Publisher = input[2],
+                ReleaseDate = DateTime.ParseExact(input[3], "dd.MM.yyyy", CultureInfo.InvariantCulture),
+                ISBN = input[4],
+                Price = double.Parse(input[5])
+            };
+
+            if (library.ContainsKey(book.Author))
+            {
+                library[book.Author] += book.Price;
+            }
+            else
+            {
+                library[book.Author] = book.Price;
+            }
+        }
+
+        library = library
+            .OrderByDescending(x => x.Value)
+            .ThenBy(x => x.Key)
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        foreach (var bk in library)
+        {
+            Console.WriteLine($"{bk.Key} -> {bk.Value:F2}");
+        }
     }
 }
