182beafaf842ecf62949e6816c8ce5e8788a9027
origami99 <origamista99@Gmail.com>
2017-07-17 18:03:16 +0300

    Solved threee tasks from "03. PHP Syntax Basic Web"

--- 03. SoftTech/03. PHP Syntax Basic Web - Exercise/01. Multiply a Number by 2.php
+++ 03. SoftTech/03. PHP Syntax Basic Web - Exercise/01. Multiply a Number by 2.php
@@ -7,8 +7,14 @@
 </head>
 <body>
 <form>
-    N: <input type="text" name="num" />
+    N: <input type="number" name="num" />
     <input type="submit" />
 </form>
+<?php
+if(isset($_GET['num'])){
+    $n = $_GET['num'];
+    echo $n * 2;
+}
+?>
 </body>
 </html>
--- 03. SoftTech/03. PHP Syntax Basic Web - Exercise/04. Product of 3 Numbers.php
+++ 03. SoftTech/03. PHP Syntax Basic Web - Exercise/04. Product of 3 Numbers.php
@@ -12,6 +12,34 @@
         Z: <input type="text" name="num3" />
 		<input type="submit" />
     </form>
-	<!--Write your PHP Script here-->
+    <?php
+    if(isset($_GET['num1']) && isset($_GET['num2']) && isset($_GET['num3'])) {
+        $x = $_GET['num1'];
+        $y = $_GET['num2'];
+        $z = $_GET['num3'];
+
+        if(substr($x, 0, 1) === '0' ||
+            substr($y, 0, 1) === '0' ||
+            substr($z, 0, 1) === '0'){
+            echo 'Positive';
+        }
+        else{
+            $negativeCount = 0;
+            if(isPositive($x))
+                $negativeCount++;
+            if(isPositive($y))
+                $negativeCount++;
+            //TODO:
+        }
+    }
+
+    function isPositive($n){
+        if (substr($n, 0, 1) === '-'){
+            return false;
+        }
+
+        return true;
+    }
+    ?>
 </body>
 </html>
