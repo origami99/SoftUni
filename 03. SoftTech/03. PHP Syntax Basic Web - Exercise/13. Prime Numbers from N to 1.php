6890f71b62830c748d991d63a917161a7b06a3d0
origami99 <origamista99@Gmail.com>
2017-07-18 14:53:27 +0300

    Solved "13. Prime numbers"

--- 03. SoftTech/03. PHP Syntax Basic Web - Exercise/13. Prime Numbers from N to 1.php
+++ 03. SoftTech/03. PHP Syntax Basic Web - Exercise/13. Prime Numbers from N to 1.php
@@ -10,6 +10,33 @@
         N: <input type="text" name="num" />
         <input type="submit" />
     </form>
-    
+    <?php
+    if(isset($_GET['num'])){
+        $n = intval($_GET['num']);
+
+        $prime_nums = array();
+
+        for($i = 2; $i <= $n; $i++){
+
+            $is_prime = true;
+            for($j = 2; $j <= $i; $j++){
+                if($i % $j === 0 && $i !== $j){
+                    $is_prime = false;
+                    break;
+                }
+            }
+
+            if($is_prime){
+                array_push($prime_nums, $i);
+            }
+        }
+
+        $prime_nums = array_reverse($prime_nums);
+
+        foreach($prime_nums as $num){
+            echo "$num ";
+        }
+    }
+    ?>
 </body>
 </html>
