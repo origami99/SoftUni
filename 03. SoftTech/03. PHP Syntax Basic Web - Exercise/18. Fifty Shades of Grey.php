9537339a611703edbaa5f14ee3f76cd6c14281c5
origami99 <origamista99@Gmail.com>
2017-07-19 05:26:01 +0300

    Solved all tasks from "03. PHP Syntax Basic Web - Exercise"

--- 03. SoftTech/03. PHP Syntax Basic Web - Exercise/18. Fifty Shades of Grey.php
+++ 03. SoftTech/03. PHP Syntax Basic Web - Exercise/18. Fifty Shades of Grey.php
@@ -1,18 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>First Steps Into PHP</title>
-    <style>
-        div {
-            display: inline-block;
-            margin: 5px;
-            width: 20px;
-            height: 20px;
+    <head>
+        <meta charset="UTF-8">
+        <title>First Steps Into PHP</title>
+        <style>
+            div {
+                display: inline-block;
+                margin: 5px;
+                width: 20px;
+                height: 20px;
+            }
+        </style>
+    </head>
+    <body>
+    <?php
+    for($row = 0; $row < 5; $row++) {
+        $shade = 51 * $row;
+
+        for ($col = 0; $col < 10; $col++) {
+            echo "<div style='background-color: rgb($shade, $shade, $shade)'></div>";
+            $shade += 5;
         }
-    </style> 
-</head>
-<body>
-<!--Write your PHP Script here-->
-</body>
+
+        echo "<br>";
+    }
+    ?>
+    </body>
 </html>
