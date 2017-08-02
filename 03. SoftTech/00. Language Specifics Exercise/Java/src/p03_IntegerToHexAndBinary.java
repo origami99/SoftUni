c6452c3459a0253ee70e4af827e3f7c7e44f6a7a
origami99 <origamista99@Gmail.com>
2017-08-02 20:44:40 +0300

    Solved on Java and JavaScript "03. Integer to Hex and Bynary"

---
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p03_IntegerToHexAndBinary.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class p03_IntegerToHexAndBinary {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        int num = Integer.parseInt(console.nextLine());
+
+        String hex = Integer.toHexString(num).toUpperCase();
+        String binary = Integer.toBinaryString(num);
+
+        System.out.println(hex);
+        System.out.println(binary);
+    }
+}
--- 03. SoftTech/00. Language Specifics Exercise/JavaScript/03. Integer to Hex and Bynary.js
+++ 03. SoftTech/00. Language Specifics Exercise/JavaScript/03. Integer to Hex and Bynary.js
@@ -1,3 +1,9 @@
 function solve(input) {
+    let num = Number(input[0]);
 
+    let hex = num.toString(16).toUpperCase();
+    let binary = num.toString(2);
+
+    console.log(hex);
+    console.log(binary);
 }
