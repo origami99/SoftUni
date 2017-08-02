6a3f06062213eabfb2f38311a4a47e7e6c4b078c
origami99 <origamista99@Gmail.com>
2017-08-02 21:56:31 +0300

    Solved on Java and JavaScript "05. Numbers in Reversed Order"

--- 03. SoftTech/00. Language Specifics Exercise/Java/src/p05_NumbersInReversedOrder.java
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p05_NumbersInReversedOrder.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class p05_NumbersInReversedOrder {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
+        String num = console.nextLine();
+        String reversed = getReversed(num);
+        System.out.println(reversed);
+    }
+
+    private static String getReversed(String num) {
+        return new StringBuilder(num).reverse().toString();
     }
 }
--- 03. SoftTech/00. Language Specifics Exercise/JavaScript/05. Numbers in Reversed Order .js
+++ 03. SoftTech/00. Language Specifics Exercise/JavaScript/05. Numbers in Reversed Order .js
@@ -1,3 +1,16 @@
 function solve(input) {
+    let num = input[0];
 
+    let reversed = getReversed(num);
+    console.log(reversed);
+
+    function getReversed(num) {
+        let reversed = "";
+
+        for(let i = num.length - 1; i >= 0; i--){
+            reversed += num[i];
+        }
+
+        return reversed;
+    }
 }
