93224b2b125f284f03130582df81ff3fdab08585
origami99 <origamista99@Gmail.com>
2017-08-02 22:23:46 +0300

    Solved on Java and JavaScript "06.Fibonacci Numbers"

--- 03. SoftTech/00. Language Specifics Exercise/JavaScript/06. Fibonacci Numbers.js
+++ 03. SoftTech/00. Language Specifics Exercise/JavaScript/06. Fibonacci Numbers.js
@@ -1,3 +1,19 @@
 function solve(input) {
+    let n = input[0];
 
+    console.log(fibonacci(n));
+
+    function fibonacci(n) {
+        let a = 0;
+        let b = 1;
+        let temp = null;
+
+        for(let i = 0; i < n; i++){
+            temp = a;
+            a = b;
+            b = temp + a;
+        }
+
+        return b;
+    }
 }
