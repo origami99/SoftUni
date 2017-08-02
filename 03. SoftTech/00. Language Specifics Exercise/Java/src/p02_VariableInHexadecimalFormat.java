d786c40506ec19c544b29ffaeeb3c79fc6f00d5a
origami99 <origamista99@Gmail.com>
2017-08-02 20:30:11 +0300

    Solved on Java and JavaScript "02. Variable in Hexadecimal Format"

--- 03. SoftTech/00. Language Specifics Exercise/Java/src/p02_VariableInHexadecimalFormat.java
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p02_VariableInHexadecimalFormat.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class p02_VariableInHexadecimalFormat {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
+        String hex = console.nextLine().substring(2);
+        int decimalNum = Integer.parseInt(hex, 16);
+        System.out.println(decimalNum);
     }
 }
--- 03. SoftTech/00. Language Specifics Exercise/Java/src/p04_EnglishNameOfTheLastDigit.java
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p04_EnglishNameOfTheLastDigit.java
@@ -1,3 +1,5 @@
 public class p04_EnglishNameOfTheLastDigit {
+    public static void main(String[] args) {
 
+    }
 }
