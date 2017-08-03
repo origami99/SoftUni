9ad3abfbec97b3638eea3975a1fe182cb0c22d07
origami99 <origamista99@Gmail.com>
2017-08-03 16:29:44 +0300

    Solved on Java and JavaScript "12. A Miner Task"

--- 03. SoftTech/00. Language Specifics Exercise/Java/src/p12_AMinerTask.java
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p12_AMinerTask.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 public class p12_AMinerTask {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
+        LinkedHashMap<String, Integer> resourses = new LinkedHashMap<>();
+
+        while (true) {
+            String res = console.nextLine();
+            if (res.equals("stop")) {
+                break;
+            }
+            Integer quant = Integer.parseInt(console.nextLine());
+
+            if (resourses.containsKey(res)) {
+                resourses.put(res, quant + resourses.get(res));
+            } else {
+                resourses.put(res, quant);
+            }
+        }
+
+        for (String ress : resourses.keySet()){
+            System.out.println(ress + " -> " + resourses.get(ress));
+        }
     }
 }
