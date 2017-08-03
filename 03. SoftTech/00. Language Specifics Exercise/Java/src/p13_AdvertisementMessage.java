4bfaf929a0372a085581b2e34615172612d0999e
origami99 <origamista99@Gmail.com>
2017-08-03 16:56:10 +0300

    Solved on Java and JavaScript "13. Advertisement Message"

--- 03. SoftTech/00. Language Specifics Exercise/Java/src/p13_AdvertisementMessage.java
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p13_AdvertisementMessage.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class p13_AdvertisementMessage {
     public static void main(String[] args) {
+        String[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product." };
+        String[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+        String[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+        String[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
 
+        Scanner console = new Scanner(System.in);
+        Integer n = Integer.parseInt(console.nextLine());
+
+        Random rnd = new Random();
+
+        for(int i = 0; i < n; i++){
+            String phr = phrases[rnd.nextInt(phrases.length)];
+            String evn = events[rnd.nextInt(events.length)];
+            String auth = authors[rnd.nextInt(authors.length)];
+            String city = cities[rnd.nextInt(cities.length)];
+
+            System.out.println(phr + " " + evn + " " + auth + " - " + city);
+        }
     }
 }
--- 03. SoftTech/00. Language Specifics Exercise/JavaScript/13. Advertisement Message.js
+++ 03. SoftTech/00. Language Specifics Exercise/JavaScript/13. Advertisement Message.js
@@ -1,3 +1,16 @@
 function solve(input) {
+    let phrases = [ "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product." ];
+    let events = [ "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" ];
+    let authors = ["Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"];
+    let cities = ["Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"];
 
+    let n = Number(input[0]);
+
+    for(let i = 0; i < n; i++){
+        let phr = phrases[Math.floor(Math.random() * phrases.length)];
+        let evn = events[Math.floor(Math.random() * events.length)];
+        let auth = authors[Math.floor(Math.random() * authors.length)];
+        let city = cities[Math.floor(Math.random() * cities.length)];
+        console.log(`${phr} ${evn} ${auth} – ${city}`);
+    }
 }
