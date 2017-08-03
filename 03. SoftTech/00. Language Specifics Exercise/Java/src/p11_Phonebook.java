3240008d2479ef4be1ff2da650c58f2876f58c12
origami99 <origamista99@Gmail.com>
2017-08-03 15:02:20 +0300

    Solved on Java and JavaScript "11. Phonebook"

--- 03. SoftTech/00. Language Specifics Exercise/Java/src/p11_Phonebook.java
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p11_Phonebook.java
@@ -1,5 +1,32 @@
+import java.util.*;
+
 public class p11_Phonebook {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
+        LinkedHashMap<String, String> phonebook = new LinkedHashMap<>();
+        while(true){
+            String[] input = console.nextLine().split(" ");
+            if(input[0].equals("END")){
+                break;
+            }
+
+            String action = input[0];
+            String name = input[1];
+
+            if(action.equals("A")){
+                String phone = input[2];
+
+                phonebook.put(name, phone);
+            }
+            else{
+                if(phonebook.containsKey(name)){
+                    System.out.println(name + " -> " + phonebook.get(name));
+                }
+                else{
+                    System.out.println("Contact " + name + " does not exist.");
+                }
+            }
+        }
     }
 }
