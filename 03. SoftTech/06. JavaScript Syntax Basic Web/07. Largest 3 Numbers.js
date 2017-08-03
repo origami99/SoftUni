71f8436f7be1a9b75f4eee8874a4047b392d1fef
origami99 <origamista99@Gmail.com>
2017-08-04 00:09:51 +0300

    Solved "07. Largest 3 Numbers"

--- 03. SoftTech/06. JavaScript Syntax Basic Web/07. Largest 3 Numbers.js
+++ 03. SoftTech/06. JavaScript Syntax Basic Web/07. Largest 3 Numbers.js
@@ -1,3 +1,18 @@
 function solve(input) {
+    let nums = input
+        .map(Number)
+        .sort(function (a, b) {
+            return b - a
+        });
 
+    let counter = 0;
+    for (let num of nums) {
+        counter++;
+
+        console.log(num);
+
+        if (counter === 3) {
+            return;
+        }
+    }
 }
