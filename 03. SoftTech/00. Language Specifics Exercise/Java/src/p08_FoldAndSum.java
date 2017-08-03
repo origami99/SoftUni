35683246b11158b9d1bc6d04885c1f970fd4aed6
origami99 <origamista99@Gmail.com>
2017-08-03 12:02:04 +0300

    Solved on Java and JavaScript "08. Fold and Sum"

--- 03. SoftTech/00. Language Specifics Exercise/Java/src/p08_FoldAndSum.java
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p08_FoldAndSum.java
@@ -1,5 +1,35 @@
+import java.util.*;
+
 public class p08_FoldAndSum {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String[] arr = console.nextLine().split(" ");
+        int[] array = Arrays.stream(arr).mapToInt(Integer::parseInt).toArray();
+        int len = arr.length;
 
+        int[] left = Arrays.copyOfRange(array, 0, len / 4);
+        reverseArray(left);
+        int[] middle = Arrays.copyOfRange(array, len / 4, len / 2 + len / 4);
+        int[] right = Arrays.copyOfRange(array, len / 2 + len / 4, len);
+        reverseArray(right);
+
+        for(int i = 0; i < left.length; i++){
+            middle[i] += left[i];
+        }
+        for(int i = 0; i < right.length; i++){
+            middle[i + len / 4] += right[i];
+        }
+
+        for (int num : middle) {
+            System.out.print(num + " ");
+        }
+    }
+
+    private static void reverseArray(int[] arr) {
+        for(int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
     }
 }
--- 03. SoftTech/00. Language Specifics Exercise/JavaScript/08. Fold and Sum.js
+++ 03. SoftTech/00. Language Specifics Exercise/JavaScript/08. Fold and Sum.js
@@ -1,3 +1,17 @@
 function solve(input) {
+    let arr = input[0].split(' ').map(Number);
+    let len = arr.length;
 
+    let left = arr.slice(0, len / 4).reverse();
+    let middle = arr.slice(len / 4, len / 2 + len / 4);
+    let right = arr.slice(len / 2 + len / 4, len).reverse();
+
+    for(let i = 0; i < left.length; i++){
+        middle[i] += left[i];
+    }
+    for(let i = 0; i < right.length; i++){
+        middle[i + len / 4] += right[i];
+    }
+
+    console.log(middle.join(' '));
 }
