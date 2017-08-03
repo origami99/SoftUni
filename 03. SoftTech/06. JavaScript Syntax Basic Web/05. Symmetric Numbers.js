ce7c0571456b074de546b89b1417f20645f73f04
origami99 <origamista99@Gmail.com>
2017-08-03 22:42:39 +0300

    Solved "05. Symmetric Numbers"

--- 03. SoftTech/06. JavaScript Syntax Basic Web/05. Symmetric Numbers.js
+++ 03. SoftTech/06. JavaScript Syntax Basic Web/05. Symmetric Numbers.js
@@ -1,5 +1,39 @@
 function solve(input) {
+    let n = input[0];
 
+    let symmetricNums = "";
+
+    for(let i = 1; i <= n; i++){
+        let leftPart = "";
+        let rightPart = "";
+        let curr = i.toString();
+        let len = curr.length;
+
+        if(i <= 9){
+            symmetricNums += curr + " ";
+            continue;
+        }
+
+        leftPart = curr.substring(0, len / 2);
+
+        len += len % 2 === 0 ? 0 : 1 ;
+        rightPart = curr.substring(len / 2);
+        rightPart = getReversed(rightPart);
+
+        if(leftPart === rightPart){
+            symmetricNums += curr + " ";
+        }
+    }
+
+    console.log(symmetricNums);
+
+    function getReversed(str) {
+        let reversed = "";
+
+        for(let i = str.length - 1; i >= 0; i--){
+            reversed += str[i];
+        }
+
+        return reversed;
+    }
 }
-
-solve([ '100' ]);
