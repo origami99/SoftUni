b9426e6da58f8bdea8fb53eac95b1564afed1617
origami99 <origamista99@Gmail.com>
2017-08-03 17:21:54 +0300

    Solved on Java and JavaScript "14. Count Substring Occurrences"

--- 03. SoftTech/00. Language Specifics Exercise/Java/src/p14_CountSubstringOccurrences.java
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p14_CountSubstringOccurrences.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class p14_CountSubstringOccurrences {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
+        String text = console.nextLine().toLowerCase();
+        String word = console.nextLine().toLowerCase();
+
+        int counter = 0;
+        int index = text.indexOf(word);
+
+        while(index != -1){
+            counter++;
+            index = text.indexOf(word, index + 1);
+        }
+
+        System.out.println(counter);
     }
 }
