1c6a3e9bddf68e9cfd59580ba57ee1807a4e8439
origami99 <origamista99@Gmail.com>
2017-08-03 20:55:33 +0300

    Solved on Java and JavaScript "17. Extract Sentences by Keyword"

--- 03. SoftTech/00. Language Specifics Exercise/Java/src/p17_ExtractSentencesByKeyword.java
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p17_ExtractSentencesByKeyword.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class p17_ExtractSentencesByKeyword {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
+        String word = console.nextLine();
+        String text = console.nextLine();
+
+        String[] sentences = text.split("[?!.]");
+        String pattern = "[^A-Za-z]" + word + "[^A-Za-z]";
+
+        Pattern r = Pattern.compile(pattern);
+
+        for (String s : sentences) {
+            Matcher m = r.matcher(s);
+
+            if (m.find()) {
+                System.out.println(s);
+            }
+        }
     }
 }
