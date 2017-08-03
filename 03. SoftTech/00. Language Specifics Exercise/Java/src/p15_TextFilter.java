1ede42c06fce521b170ddb5003e98f13e0e06051
origami99 <origamista99@Gmail.com>
2017-08-03 18:25:49 +0300

    Solved on Java and JavaScript "15. Text Filter"

--- 03. SoftTech/00. Language Specifics Exercise/Java/src/p15_TextFilter.java
+++ 03. SoftTech/00. Language Specifics Exercise/Java/src/p15_TextFilter.java
@@ -1,5 +1,18 @@
+import java.util.Collections;
+import java.util.Scanner;
+
 public class p15_TextFilter {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
+        String[] bannedWords = console.nextLine().split(", ");
+        String text = console.nextLine();
+
+        for (String bannedWord : bannedWords) {
+            String stars = String.join("", Collections.nCopies(bannedWord.length(), "*"));
+            text = text.replace(bannedWord, stars);
+        }
+
+        System.out.println(text);
     }
 }
