b64c349d84485f7d73a5beb82d649378a9163303
origami99 <origamista99@Gmail.com>
2017-08-04 15:34:55 +0300

    Solved "12. Multiple Values for a Key"

--- 03. SoftTech/07. JavaScript Basics - Exercises/12. Multiple Values for a Key.js
+++ 03. SoftTech/07. JavaScript Basics - Exercises/12. Multiple Values for a Key.js
@@ -1,5 +1,25 @@
 function solve(input) {
+    let dict = new Map;
 
+    for (let i = 0; i < input.length - 1; i++) {
+        let key = input[i].split(' ')[0];
+        let value = input[i].split(' ')[1];
+
+        if (!(key in dict)){
+            dict[key] = [];
+        }
+
+        dict[key].push(value);
+    }
+
+    let searched = input[input.length - 1];
+
+    if(searched in dict){
+        console.log(dict[searched].join('\n'));
+    }
+    else{
+        console.log("None");
+    }
 }
 
-solve([ '' ]);
+solve([ 'key value', 'key eulav', 'test tset', 'key' ]);
