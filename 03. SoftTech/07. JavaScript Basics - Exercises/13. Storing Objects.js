eca771aeccb40a0c00cb11c8264b2962faab63ad
origami99 <origamista99@Gmail.com>
2017-08-04 15:56:28 +0300

    Solved "13. Storing Objects"

--- 03. SoftTech/07. JavaScript Basics - Exercises/13. Storing Objects.js
+++ 03. SoftTech/07. JavaScript Basics - Exercises/13. Storing Objects.js
@@ -1,5 +1,25 @@
 function solve(input) {
+    class Student {
+        constructor(name, age, grade) {
+            this.name = name;
+            this.age = age;
+            this.grade = grade;
+        }
+    }
 
+    for (let i = 0; i < input.length; i++) {
+        let studentData = input[i].split(" -> ");
+        let name = studentData[0];
+        let age = studentData[1];
+        let grade = studentData[2];
+
+        let student = new Student();
+        student.name = name;
+        student.age = age;
+        student.grade = grade;
+
+        console.log(`Name: ${student.name}`);
+        console.log(`Age: ${student.age}`);
+        console.log(`Grade: ${student.grade}`);
+    }
 }
-
-solve([ '' ]);
