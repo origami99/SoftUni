a602648101c7476bc5e55662f38700feba6e6470
origami99 <origamista99@Gmail.com>
2017-08-04 14:40:16 +0300

    Solved "10. Add or Remove Elements" and "11. Working with Key-Value Pairs"

--- 03. SoftTech/07. JavaScript Basics - Exercises/10. Add or Remove Elements.js
+++ 03. SoftTech/07. JavaScript Basics - Exercises/10. Add or Remove Elements.js
@@ -1,5 +1,20 @@
 function solve(input) {
+    let list = [];
 
+    for (let i = 0; i < input.length; i++) {
+        let command = input[i].split(' ');
+
+        switch (command[0]) {
+            case "add":
+                let num = command[1];
+                list.push(num);
+                break;
+            case "remove":
+                let index = command[1];
+                list.splice(index, 1);
+                break;
+        }
+    }
+
+    return list.join("\n");
 }
-
-solve([ '' ]);
