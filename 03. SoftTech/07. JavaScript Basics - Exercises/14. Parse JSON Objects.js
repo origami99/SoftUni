7c0a028c2262d98697c3379aec462d0d8bacf0d2
origami99 <origamista99@Gmail.com>
2017-08-04 16:55:44 +0300

    Solved "14. Parse JSON Objects" and "15. Turn Object into JSON String"

--- 03. SoftTech/07. JavaScript Basics - Exercises/14. Parse JSON Objects.js
+++ 03. SoftTech/07. JavaScript Basics - Exercises/14. Parse JSON Objects.js
@@ -1,5 +1,9 @@
 function solve(input) {
+    for(let json of input){
+        let obj = JSON.parse(json);
 
+        console.log(`Name: ${obj.name}`);
+        console.log(`Age: ${obj.age}`);
+        console.log(`Date: ${obj.date}`);
+    }
 }
-
-solve([ '' ]);
