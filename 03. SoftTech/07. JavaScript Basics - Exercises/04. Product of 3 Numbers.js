f52f6eab16799b21f82faa7e2b786fd2900ee9eb
origami99 <origamista99@Gmail.com>
2017-08-04 12:35:01 +0300

    Solved "04. Product of 3 Numbers"

--- 03. SoftTech/07. JavaScript Basics - Exercises/04. Product of 3 Numbers.js
+++ 03. SoftTech/07. JavaScript Basics - Exercises/04. Product of 3 Numbers.js
@@ -1,5 +1,7 @@
 function solve(input) {
+    let x = Number(input[0]);
+    let y = Number(input[1]);
+    let z = Number(input[2]);
 
+    console.log([x, y, z].filter(x => x < 0).length % 2 === 0 ? "Positive" : "Negative");
 }
-
-solve([ '' ]);
