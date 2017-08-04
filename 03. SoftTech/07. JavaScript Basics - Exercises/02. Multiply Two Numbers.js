70ca0b43f6dd21f82e8e3ac8ae434e3f05bfc3c4
origami99 <origamista99@Gmail.com>
2017-08-04 12:11:03 +0300

    Solved "02. Multiply Two Numbers" and "03. Multiply or Divide a Number"

--- 03. SoftTech/07. JavaScript Basics - Exercises/02. Multiply Two Numbers.js
+++ 03. SoftTech/07. JavaScript Basics - Exercises/02. Multiply Two Numbers.js
@@ -1,5 +1,5 @@
 function solve(input) {
-
+    let x = Number(input[0]);
+    let y = Number(input[1]);
+    return x * y;
 }
-
-solve([ '' ]);
--- 03. SoftTech/07. JavaScript Basics - Exercises/03. Multiply or Divide a Number.js
+++ 03. SoftTech/07. JavaScript Basics - Exercises/03. Multiply or Divide a Number.js
@@ -1,5 +1,6 @@
 function solve(input) {
+    let n = Number(input[0]);
+    let x = Number(input[1]);
 
+    return x >= n ? (n * x) : (n / x);
 }
-
-solve([ '' ]);
