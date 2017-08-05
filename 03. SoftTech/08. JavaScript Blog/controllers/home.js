5adb37c2f66bbcd8d8bb2687c2430847f050c4b9
origami99 <origamista99@Gmail.com>
2017-08-05 17:23:19 +0300

    Added the ready ExpressJS MVC blog

--- 03. SoftTech/08. JavaScript Blog/controllers/home.js
+++ 03. SoftTech/08. JavaScript Blog/controllers/home.js
@@ -1,5 +1,10 @@
+const mongoose = require('mongoose');
+const Article = mongoose.model('Article');
+
 module.exports = {
   index: (req, res) => {
-      res.render('home/index');
+      Article.find({}).limit(6).populate('author').then(articles => {
+          res.render('home/index', {articles: articles});
+      })
   }
 };
