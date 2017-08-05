1b07263b8ede266f91c6cdb7e7a54afccc502f59
origami99 <origamista99@Gmail.com>
2017-08-05 13:26:12 +0300

    Finished the Calculator ExpressJS MVC

--- 03. SoftTech/08. JavaScript Calculator/config/routes.js
+++ 03. SoftTech/08. JavaScript Calculator/config/routes.js
@@ -2,5 +2,5 @@ const homeController = require('./../controllers/home');
 
 module.exports = (app) => {
     app.get('/', homeController.indexGet);
+    app.post('/', homeController.indexPost);
 };
-
