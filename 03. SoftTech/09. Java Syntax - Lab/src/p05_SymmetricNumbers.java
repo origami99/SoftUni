371430381e8226de6c5c19403216f49b495b1a34
origami99 <origamista99@Gmail.com>
2017-08-05 23:56:45 +0300

    Solved "05. Symmetric Numbers"

--- 03. SoftTech/09. Java Syntax - Lab/src/p05_SymmetricNumbers.java
+++ 03. SoftTech/09. Java Syntax - Lab/src/p05_SymmetricNumbers.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class p05_SymmetricNumbers {
 
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
+        int n = Integer.parseInt(console.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            if (isSymmetric(Integer.toString(i))){
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    private static boolean isSymmetric(String str) {
+        for (int j = 0; j < str.length() / 2; j++) {
+            if (str.charAt(j) != str.charAt(str.length() - 1 - j)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
