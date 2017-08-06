7e044376becef04f97e03e4e1709af70e08edfc9
origami99 <origamista99@Gmail.com>
2017-08-06 22:24:37 +0300

    Solved on Java "10. Index of Letters"

--- 03. SoftTech/09. Java Syntax - Exercises/src/p10_IndexOfLetters.java
+++ 03. SoftTech/09. Java Syntax - Exercises/src/p10_IndexOfLetters.java
@@ -5,5 +5,10 @@ public class p10_IndexOfLetters {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        char[] word = console.nextLine().toCharArray();
+
+        for (char letter : word) {
+            System.out.println(letter + " -> " + (letter - 'a'));
+        }
     }
 }
