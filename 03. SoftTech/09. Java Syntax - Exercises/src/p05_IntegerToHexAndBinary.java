1bd41437d703b338995569e13636fd7846fe6bd2
origami99 <origamista99@Gmail.com>
2017-08-06 11:46:18 +0300

    Solved on Java "05. integer to Hex and Binary" and "06. Compare Char Arrays"

--- 03. SoftTech/09. Java Syntax - Exercises/src/p05_IntegerToHexAndBinary.java
+++ 03. SoftTech/09. Java Syntax - Exercises/src/p05_IntegerToHexAndBinary.java
@@ -5,5 +5,12 @@ public class p05_IntegerToHexAndBinary {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        int num = Integer.parseInt(console.nextLine());
+
+        String hex = Integer.toHexString(num).toUpperCase();
+        String binary = Integer.toBinaryString(num);
+
+        System.out.println(hex);
+        System.out.println(binary);
     }
 }
