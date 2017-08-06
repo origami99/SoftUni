d5db312685c42324187df37d610b69071eaf2f7f
origami99 <origamista99@Gmail.com>
2017-08-06 21:37:02 +0300

    Solved on Java "07. Max Sequence of Equal Elements" and "08. max Sequence Of Increasing Elements"

--- 03. SoftTech/09. Java Syntax - Exercises/src/p07_MaxSequenceOfEqualElements.java
+++ 03. SoftTech/09. Java Syntax - Exercises/src/p07_MaxSequenceOfEqualElements.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.Scanner;
 
 public class p07_MaxSequenceOfEqualElements {
@@ -5,5 +6,34 @@ public class p07_MaxSequenceOfEqualElements {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        String[] nums = console.nextLine().split(" ");
+
+        String currElem = "";
+        String nextElem = "";
+        int currLen = 1;
+        int bestLen = 0;
+        String bestNum = "";
+
+        for (int i = 0; i < nums.length; i++) {
+            currElem = nums[i];
+
+            if (i != nums.length - 1){
+                nextElem = nums[i + 1];
+
+                if (currElem.equals(nextElem)){
+                    currLen++;
+                }
+                else{
+                    currLen = 1;
+                }
+            }
+
+            if (currLen > bestLen){
+                bestLen = currLen;
+                bestNum = nums[i];
+            }
+        }
+
+        System.out.println(String.join(" ", Collections.nCopies(bestLen, bestNum)));
     }
 }
--- 03. SoftTech/09. Java Syntax - Exercises/src/p09_MostFrequentNumber.java
+++ 03. SoftTech/09. Java Syntax - Exercises/src/p09_MostFrequentNumber.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class p09_MostFrequentNumber {
@@ -5,5 +6,10 @@ public class p09_MostFrequentNumber {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        Integer[] nums = Arrays.stream(console.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
+
+        
     }
 }
