f73dadc3dfd43e39c9a86e58445dc00cc76c7cb3
origami99 <origamista99@Gmail.com>
2017-08-06 22:10:04 +0300

    Solved on Java "09. Most Frequent Number"

--- 03. SoftTech/09. Java Syntax - Exercises/src/p09_MostFrequentNumber.java
+++ 03. SoftTech/09. Java Syntax - Exercises/src/p09_MostFrequentNumber.java
@@ -1,15 +1,30 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class p09_MostFrequentNumber {
 
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
-        Integer[] nums = Arrays.stream(console.nextLine().split(" "))
-                .map(Integer::parseInt)
-                .toArray(Integer[]::new);
+        String[] nums = console.nextLine().split(" ");
 
-        
+        LinkedHashMap<String, Integer> count = new LinkedHashMap<>();
+        for (String num : nums) {
+            if (!count.containsKey(num)){
+                count.put(num, 1);
+            }
+
+            count.put(num, count.get(num) + 1);
+        }
+
+        String mostOccurNum = null;
+        Integer maxVal = 0;
+        for (Map.Entry<String, Integer> item : count.entrySet()) {
+            if (item.getValue() > maxVal){
+                maxVal = item.getValue();
+                mostOccurNum = item.getKey();
+            }
+        }
+
+        System.out.println(mostOccurNum);
     }
 }
