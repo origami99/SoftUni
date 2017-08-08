93e9d2c61777a8a73dafb12e73b1021e7b778b30
origami99 <origamista99@Gmail.com>
2017-08-08 15:47:37 +0300

    Solved on Java "13. Reverse String"

--- 03. SoftTech/09. Java Syntax - Exercises/src/p13_ReverseString.java
+++ 03. SoftTech/09. Java Syntax - Exercises/src/p13_ReverseString.java
@@ -5,5 +5,10 @@ public class p13_ReverseString {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        String[] arr = console.nextLine().split("");
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
     }
 }
