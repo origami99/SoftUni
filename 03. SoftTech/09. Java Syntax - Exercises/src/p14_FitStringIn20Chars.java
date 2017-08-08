03f35ef698adcaa661dc3f4be19b5be17e9dfaef
origami99 <origamista99@Gmail.com>
2017-08-08 17:44:14 +0300

    Solved "14. Fit String in 20 Chars"

--- 03. SoftTech/09. Java Syntax - Exercises/src/p14_FitStringIn20Chars.java
+++ 03. SoftTech/09. Java Syntax - Exercises/src/p14_FitStringIn20Chars.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.Scanner;
 
 public class p14_FitStringIn20Chars {
@@ -5,5 +6,13 @@ public class p14_FitStringIn20Chars {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        String str = console.nextLine();
+
+        if (str.length() <= 20) {
+            String stars = String.join("", Collections.nCopies(20 - str.length(), "*"));
+            System.out.println(str + stars);
+        } else {
+            System.out.println(str.substring(0, 20));
+        }
     }
 }
