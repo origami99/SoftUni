15030a292be8995e2785abcc26c5af6511914c85
origami99 <origamista99@Gmail.com>
2017-08-08 18:02:08 +0300

    Solved on Java "15. Censor Email Address"

--- 03. SoftTech/09. Java Syntax - Exercises/src/p15_CensorEmailAddress.java
+++ 03. SoftTech/09. Java Syntax - Exercises/src/p15_CensorEmailAddress.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.Scanner;
 
 public class p15_CensorEmailAddress {
@@ -5,5 +6,15 @@ public class p15_CensorEmailAddress {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        String email = console.nextLine();
+        String text = console.nextLine();
+
+        String username = email.split("@")[0];
+        String domain = email.split("@")[1];
+
+        String stars = String.join("", Collections.nCopies(username.length(), "*"));
+        text = text.replaceAll(email, stars + "@" + domain);
+
+        System.out.println(text);
     }
 }
