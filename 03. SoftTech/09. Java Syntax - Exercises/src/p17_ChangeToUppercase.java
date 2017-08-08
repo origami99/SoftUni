ff50c091a2115c2d81fbbb901a56474c2802dabb
origami99 <origamista99@Gmail.com>
2017-08-08 19:17:23 +0300

    Solved on Java "17. Change to Uppercase"

--- 03. SoftTech/09. Java Syntax - Exercises/src/p17_ChangeToUppercase.java
+++ 03. SoftTech/09. Java Syntax - Exercises/src/p17_ChangeToUppercase.java
@@ -5,5 +5,18 @@ public class p17_ChangeToUppercase {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        String text = console.nextLine();
+
+        while (text.contains("<upcase>")){
+            int startIndex = text.indexOf("<upcase>");
+            int endIndex = text.indexOf("</upcase>") + 9;
+
+            String tagStatement = text.substring(startIndex, endIndex);
+            String upperText = text.substring(startIndex + 8, endIndex - 9).toUpperCase();
+
+            text = text.replace(tagStatement, upperText);
+        }
+
+        System.out.println(text);
     }
 }
