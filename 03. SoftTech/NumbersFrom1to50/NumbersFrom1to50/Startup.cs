6d84c667d6929dc242c1575f09753dfe050c00d0
origami99 <origamista99@Gmail.com>
2017-08-16 13:26:28 +0300

    Added and finished the "Numbers From 1 to 50" ASP.NET MVC

---
+++ 03. SoftTech/NumbersFrom1to50/NumbersFrom1to50/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NumbersFrom1to50.Startup))]
+namespace NumbersFrom1to50
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
