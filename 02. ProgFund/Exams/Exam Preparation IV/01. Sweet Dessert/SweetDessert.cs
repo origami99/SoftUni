3d3f59883962c12ac25626db2bd941a0b4f0e695
origami99 <origamista99@Gmail.com>
2017-08-17 16:53:30 +0300

    Solved ""

--- 02. ProgFund/Exams/Exam Preparation IV/01. Sweet Dessert/SweetDessert.cs
+++ 02. ProgFund/Exams/Exam Preparation IV/01. Sweet Dessert/SweetDessert.cs
@@ -4,6 +4,25 @@ class SweetDessert
 {
     static void Main()
     {
+        double cash = double.Parse(Console.ReadLine());
+        int guestsCount = int.Parse(Console.ReadLine());
+        double bananaPrice = double.Parse(Console.ReadLine());
+        double eggPrice = double.Parse(Console.ReadLine());
+        double berriesKiloPrice = double.Parse(Console.ReadLine());
 
+        int setsCount = guestsCount / 6;
+        if (guestsCount % 6 != 0) { setsCount++; }
+
+        double setCost = 2 * bananaPrice + 4 * eggPrice + 0.2 * berriesKiloPrice;
+        double totalCost = setsCount * setCost;
+
+        if (totalCost <= cash)
+        {
+            Console.WriteLine($"Ivancho has enough money - it would cost {totalCost:F2}lv.");
+        }
+        else if (totalCost > cash)
+        {
+            Console.WriteLine($"Ivancho will have to withdraw money - he will need {totalCost - cash:F2}lv more.");
+        }
     }
 }
