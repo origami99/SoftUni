49a2945a5096db2a8c13a0d1de4aa7deceb1610a
origami99 <origamista99@Gmail.com>
2017-08-18 19:34:18 +0300

    Finished 02. Worm Ipsum

--- 02. ProgFund/Exams/Prog Fund (Extended) Exam - 30 April 2017/02. Worm Ipsum/WormIpsum.cs
+++ 02. ProgFund/Exams/Prog Fund (Extended) Exam - 30 April 2017/02. Worm Ipsum/WormIpsum.cs
@@ -1,9 +1,36 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-class WormIpsum
+public class WormIpsum
 {
-    static void Main()
+    public static void Main()
     {
+        Regex sentenceReg = new Regex(@"^[A-Z][^\.]*\.$");
+        Regex wordsReg = new Regex(@"[^\s,\.]+");
 
+        string input = Console.ReadLine();
+        while (!input.Equals("Worm Ipsum"))
+        {
+            Match sentenceMatch = sentenceReg.Match(input);
+            if (sentenceMatch.Success)
+            {
+                MatchCollection wordsMatches = wordsReg.Matches(input);
+                foreach (Match match in wordsMatches)
+                {
+                    string word = match.Value;
+                    if (word.Length != word.Distinct().Count())
+                    {
+                        char symb = word.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+                        string newWord = string.Concat(Enumerable.Repeat(symb, word.Length));
+                        input = Regex.Replace(input, word, newWord);
+                    }
+                }
+
+                Console.WriteLine(input);
+            }
+
+            input = Console.ReadLine();
+        }
     }
 }
