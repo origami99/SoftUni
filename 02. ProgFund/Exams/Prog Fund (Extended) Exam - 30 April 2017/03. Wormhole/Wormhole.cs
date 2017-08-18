adab34710a17d7c15bc9602e6e3de428a5cb22fa
origami99 <origamista99@Gmail.com>
2017-08-18 22:50:12 +0300

    Solved "04. Wormhole"

--- 02. ProgFund/Exams/Prog Fund (Extended) Exam - 30 April 2017/03. Wormhole/Wormhole.cs
+++ 02. ProgFund/Exams/Prog Fund (Extended) Exam - 30 April 2017/03. Wormhole/Wormhole.cs
@@ -1,9 +1,30 @@
 ﻿using System;
+using System.Linq;
 
 class Wormhole
 {
     static void Main()
     {
+        int[] holes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
+        int steps = 1;
+        for (int i = 0; i < holes.Length; i++)
+        {
+            if (holes[i] == 0)
+            {
+                if (i != holes.Length - 1)
+                {
+                    steps++;
+                }
+            }
+            else
+            {
+                int temp = i;
+                i = holes[i] - 1;
+                holes[temp] = 0;
+            }
+        }
+
+        Console.WriteLine(steps);
     }
 }
