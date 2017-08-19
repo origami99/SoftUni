34000fcc018b85c4b3d74d2c3ef2377ab5de984e
origami99 <origamista99@Gmail.com>
2017-08-19 11:17:59 +0300

    Solved "01. Poke Mon"

---
+++ 02. ProgFund/Exams/Prog Fund Exam - 09 July 2017 Part 1/01. Poke Mon/PokeMon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class PokeMon
+{
+    static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+        long m = long.Parse(Console.ReadLine());
+        long y = long.Parse(Console.ReadLine());
+
+        long initialPower = n;
+        int counter = 0;
+
+        while (!(n < m))
+        {
+            n -= m;
+            counter++;
+
+            if (((decimal)n / initialPower) * 100.0m == 50.0m)
+            {
+                if (y != 0)
+                {
+                    n /= y;
+                }
+                else
+                {
+                    n -= m;
+                    counter++;
+                }
+            }
+        }
+
+        Console.WriteLine(n);
+        Console.WriteLine(counter);
+    }
+}
