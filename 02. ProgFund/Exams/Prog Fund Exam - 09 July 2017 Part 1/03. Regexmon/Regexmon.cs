f50ceaf3188dcbfcef8a812658f098bf170eb131
origami99 <origamista99@Gmail.com>
2017-08-19 16:27:34 +0300

    Solved "03. Regexmon"

--- 02. ProgFund/Exams/Prog Fund Exam - 09 July 2017 Part 1/03. Regexmon/Regexmon.cs
+++ 02. ProgFund/Exams/Prog Fund Exam - 09 July 2017 Part 1/03. Regexmon/Regexmon.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
-class Program
+class Regexmon
 {
     static void Main()
     {
+        string text = Console.ReadLine();
 
+        Regex bojoRegex = new Regex(@"[A-Za-z]+\-[A-Za-z]+");
+        Regex didiRegex = new Regex(@"[^A-Za-z\-]+");
+
+        int lastIndex = 0;
+        while (true)
+        {
+            Match didiMatch = didiRegex.Match(text);
+            if (!didiMatch.Success) { return; }
+            lastIndex = text.IndexOf(didiMatch.Value) + didiMatch.Length;
+            text = text.Substring(lastIndex);
+            Console.WriteLine(didiMatch);
+
+            Match bojoMatch = bojoRegex.Match(text);
+            if (!bojoMatch.Success) { return; }
+            lastIndex = text.IndexOf(bojoMatch.Value) + bojoMatch.Length;
+            text = text.Substring(lastIndex);
+            Console.WriteLine(bojoMatch);
+        }
     }
 }
