67f13020571684c4ea48215c9b25705cb0ac54f1
origami99 <origamista99@gmail.com>
2018-06-02 23:26:48 +0300

    Solved new task - "Periodic Table"

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/03. Periodic Table/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/03. Periodic Table/Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        SortedSet<string> elements = new SortedSet<string>();
+        for (int i = 0; i < n; i++)
+        {
+            string[] copounds = Console.ReadLine().Split(' ');
+
+            foreach (var element in copounds)
+            {
+                elements.Add(element);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", elements));
     }
 }
