9443fb663be475e7c8603322273c00904dd05345
origami99 <origamista99@gmail.com>
2018-06-02 22:58:58 +0300

    Solved  the "Sets of Elements" task

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/02. Sets of Elements/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/02. Sets of Elements/Program.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
+        string input = Console.ReadLine();
+        int n = int.Parse(input.Substring(0, 1));
+        int m = int.Parse(input.Substring(2, 1));
 
+        HashSet<int> firstSet = new HashSet<int>();
+        for (int i = 0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            firstSet.Add(num);
+        }
+
+        HashSet<int> secondSet = new HashSet<int>();
+        for (int i = 0; i < m; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            secondSet.Add(num);
+        }
+
+        HashSet<int> lastSet = new HashSet<int>();
+        foreach (int num in firstSet)
+        {
+            if (secondSet.Contains(num))
+            {
+                lastSet.Add(num);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", lastSet));
     }
 }
