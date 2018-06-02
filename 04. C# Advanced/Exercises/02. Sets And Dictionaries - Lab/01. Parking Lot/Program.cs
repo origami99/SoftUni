be25bfbc28d0e356d0a43c3e5b4f8ff61ac77abb
origami99 <origamista99@gmail.com>
2018-06-02 18:18:09 +0300

    Solved the "Parking Lot" task

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Lab/01. Parking Lot/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Lab/01. Parking Lot/Program.cs
@@ -1,9 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        HashSet<string> cars = new HashSet<string>();
 
+        while (true)
+        {
+            string[] input = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            if (input[0] == "END") { break; }
+
+            string command = input[0];
+            string car = input[1];
+
+            if (command == "IN")
+            {
+                cars.Add(car);
+            }
+            else if (command == "OUT")
+            {
+                cars.Remove(car);
+            }
+        }
+
+        if (!cars.Any())
+        {
+            Console.WriteLine("Parking Lot is Empty");
+            return;
+        }
+
+        Console.WriteLine(string.Join("\n", cars));
     }
 }
