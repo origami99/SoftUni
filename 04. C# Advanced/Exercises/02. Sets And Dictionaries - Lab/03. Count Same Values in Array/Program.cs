b5d3d5403206680307e5d5498a8ecd264b9fd4b0
origami99 <origamista99@gmail.com>
2018-06-02 19:14:59 +0300

    Solved  the "Count Same Values in Array" task

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Lab/03. Count Same Values in Array/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Lab/03. Count Same Values in Array/Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        string[] values = Console.ReadLine().Split(' ');
 
+        SortedDictionary<string, int> occurs = new SortedDictionary< string, int>();
+
+        foreach (string value in values)
+        {
+            if (!occurs.ContainsKey(value))
+            {
+                occurs.Add(value , 1);
+            }
+            else
+            {
+                occurs[value]++;
+            }
+        }
+
+        string[] result = occurs.Select(x => x.Key + " - " + x.Value + " times").ToArray();
+        Console.WriteLine(string.Join("\n", result));
     }
 }
