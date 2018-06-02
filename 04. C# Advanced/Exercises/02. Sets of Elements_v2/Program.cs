acb1fbb907a7a9b60d73498b783fb659f30137db
origami99 <origamista99@gmail.com>
2018-06-02 23:10:22 +0300

    Solved  the "Sets of Elements_2" task

---
+++ 04. C# Advanced/Exercises/02. Sets of Elements_v2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int n = int.Parse(input.Substring(0, 1));
+        int m = int.Parse(input.Substring(2, 1));
+
+        HashSet<int> firstSet = new HashSet<int>();
+        for (int i = 0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            firstSet.Add(num);
+        }
+
+        HashSet<int> secondSet = new HashSet<int>();
+        for (int i = 0; i < m; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            secondSet.Add(num);
+        }
+
+        firstSet.IntersectWith(secondSet);
+
+        Console.WriteLine(string.Join(" ", firstSet));
+    }
+}
