96d302f4fe319a080fdfbfb289e4e749c70e9a1e
origami99 <origamista99@gmail.com>
2018-06-02 23:33:29 +0300

    Solved new task - "Count Symbols"

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/04. Count Symbols/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/04. Count Symbols/Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
+        string text = Console.ReadLine();
 
+        SortedDictionary<char, int> occurs = new SortedDictionary<char, int>();
+        foreach (char symb in text)
+        {
+            if (!occurs.ContainsKey(symb))
+            {
+                occurs.Add(symb, 1);
+            }
+            else
+            {
+                occurs[symb]++;
+            }
+        }
+
+        foreach (KeyValuePair<char, int> occur in occurs)
+        {
+            Console.WriteLine($"{occur.Key}: {occur.Value} time/s");
+        }
     }
 }
