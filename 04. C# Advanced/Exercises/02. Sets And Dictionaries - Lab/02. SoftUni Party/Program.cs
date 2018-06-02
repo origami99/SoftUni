28f4b289781247353a773484433941532855f14f
origami99 <origamista99@gmail.com>
2018-06-02 18:53:57 +0300

    Added solution on "SoftUni Party"

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Lab/02. SoftUni Party/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Lab/02. SoftUni Party/Program.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        SortedSet<string> guest = new SortedSet<string>();
 
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "PARTY") { break; }
+
+            guest.Add(input);
+        }
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "END") { break; }
+
+            guest.Remove(input);
+        }
+
+        Console.WriteLine(string.Join("\n", guest));
     }
 }
