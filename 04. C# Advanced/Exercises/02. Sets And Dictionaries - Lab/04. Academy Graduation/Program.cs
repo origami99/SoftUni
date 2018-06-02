0131c1cf8296cbc13251b4c5b5c64fd2881253f4
origami99 <origamista99@gmail.com>
2018-06-02 19:31:13 +0300

    Solved  the "Academy Graduation" task

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Lab/04. Academy Graduation/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Lab/04. Academy Graduation/Program.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        SortedDictionary<string, double> students = new SortedDictionary<string, double>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string name = Console.ReadLine();
+            double score = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => double.Parse(x))
+                .Average();
+
+            students.Add(name, score);
+        }
+
+        foreach (var student in students)
+        {
+            Console.WriteLine($"{student.Key} is graduated with {student.Value}");
+        }
     }
 }
