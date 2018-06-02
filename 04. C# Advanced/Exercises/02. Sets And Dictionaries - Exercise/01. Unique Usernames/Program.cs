0db242e5d8905b98fdc0991dfc7907a198d9a1ad
origami99 <origamista99@gmail.com>
2018-06-02 22:30:57 +0300

    Solved  the "Unique Usernames" task

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/01. Unique Usernames/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/01. Unique Usernames/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        HashSet<string> usernames = new HashSet<string>();
+        for (int i = 0; i < 6; i++)
+        {
+            string username = Console.ReadLine();
+
+            usernames.Add(username);
+        }
+
+        Console.WriteLine(string.Join("\n", usernames));
     }
 }
