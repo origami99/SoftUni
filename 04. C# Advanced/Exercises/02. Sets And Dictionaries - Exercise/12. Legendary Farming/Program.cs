369292c8f59b3efeb14fc2d1576a52522cc5cee9
origami99 <origamista99@gmail.com>
2018-06-03 16:12:30 +0300

    Added and solved a bunch of new tasks

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/12. Legendary Farming/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/12. Legendary Farming/Program.cs
@@ -1,10 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-class Program
+class LegendaryFarming
 {
     static void Main()
     {
+        Dictionary<string, int> materials = new Dictionary<string, int>()
+        {
+            { "shards", 0 },
+            { "fragments", 0 },
+            { "motes", 0 }
+        };
+        while (true)
+        {
+            string[] resourse;
+            int[] quantities;
+            ReceiveMaterials(out resourse, out quantities);
 
+            for (int i = 0; i < resourse.Length; i++)
+            {
+                string item = resourse[i];
+                int quantity = quantities[i];
+
+                if (materials.ContainsKey(item))
+                {
+                    materials[item] += quantity;
+                }
+                else
+                {
+                    materials[item] = quantity;
+                }
+
+                if (materials["shards"] >= 250 ||
+                    materials["fragments"] >= 250 ||
+                    materials["motes"] >= 250)
+                {
+                    goto Exit;
+                }
+            }
+        }
+        Exit:
+
+        PrintObtainedLegendaryItem(materials);
+
+        materials = OrderByValueThenByName(materials);
+
+        PrintKeyMaterials(materials);
+
+        RemoveKeyMaterials(materials);
+
+        materials = OrderByName(materials);
+
+        PrintJunkMaterials(materials);
+    }
+
+    private static void ReceiveMaterials(out string[] resourse, out int[] quantities)
+    {
+        string[] loot = Console.ReadLine()
+            .Split(' ')
+            .ToArray();
+
+        resourse = loot
+            .Where((x, i) => i % 2 != 0)
+            .Select(x => x.ToLower())
+            .ToArray();
+        quantities = loot
+            .Where((x, i) => i % 2 == 0)
+            .Select(x => int.Parse(x))
+            .ToArray();
+    }
+
+    private static void PrintObtainedLegendaryItem(Dictionary<string, int> materials)
+    {
+        if (materials["shards"] >= 250)
+        {
+            Console.WriteLine("Shadowmourne obtained!");
+            materials["shards"] -= 250;
+        }
+        else if (materials["fragments"] >= 250)
+        {
+            Console.WriteLine("Valanyr obtained!");
+            materials["fragments"] -= 250;
+        }
+        else if (materials["motes"] >= 250)
+        {
+            Console.WriteLine("Dragonwrath obtained!");
+            materials["motes"] -= 250;
+        }
+    }
+
+    private static Dictionary<string, int> OrderByValueThenByName(Dictionary<string, int> materials)
+    {
+        materials = materials
+            .OrderByDescending(x => x.Value)
+            .ThenBy(x => x.Key)
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        return materials;
+    }
+
+    private static void PrintKeyMaterials(Dictionary<string, int> materials)
+    {
+        foreach (var item in materials)
+        {
+            if (item.Key == "shards" ||
+                item.Key == "fragments" ||
+                item.Key == "motes")
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+
+    private static void RemoveKeyMaterials(Dictionary<string, int> materials)
+    {
+        materials.Remove("shards");
+        materials.Remove("fragments");
+        materials.Remove("motes");
+    }
+
+    private static Dictionary<string, int> OrderByName(Dictionary<string, int> materials)
+    {
+        materials = materials
+            .OrderBy(x => x.Key)
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        return materials;
+    }
+
+    private static void PrintJunkMaterials(Dictionary<string, int> materials)
+    {
+        foreach (var junk in materials)
+        {
+            Console.WriteLine($"{junk.Key}: {junk.Value}");
+        }
     }
 }
--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/14. Dragon Army/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/14. Dragon Army/Program.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        Dictionary<string, SortedDictionary<string, int[]>> dragons = new Dictionary<string, SortedDictionary<string, int[]>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] dragon = Console.ReadLine().Split(' ');
+
+            string type = dragon[0];
+            string name = dragon[1];
+            int damage = dragon[2] == "null" ? 45 : int.Parse(dragon[2]);
+            int health = dragon[3] == "null" ? 250 : int.Parse(dragon[3]);
+            int armor = dragon[4] == "null" ? 10 : int.Parse(dragon[4]);
+            int[] stats = new int[] { damage, health, armor };
+
+            if (!dragons.ContainsKey(type))
+            {
+                dragons.Add(type, new SortedDictionary<string, int[]>() { { name, stats } });
+            }
+            else
+            {
+                dragons[type][name] = stats;
+            }
+        }
+
+        foreach (KeyValuePair<string, SortedDictionary<string, int[]>> dragon in dragons)
+        {
+            double avgDamage = dragon.Value.Values.Average(x => x[0]);
+            double avgHealth = dragon.Value.Values.Average(x => x[1]);
+            double avgArmor = dragon.Value.Values.Average(x => x[2]);
+
+            Console.WriteLine($"{dragon.Key}::({avgDamage:F2}/{avgHealth:F2}/{avgArmor:F2})");
+
+            foreach (KeyValuePair<string, int[]> item in dragon.Value)
+            {
+                Console.WriteLine($"-{item.Key} -> damage: {item.Value[0]}, health: {item.Value[1]}, armor: {item.Value[2]}");
+            }
+        }
     }
 }
