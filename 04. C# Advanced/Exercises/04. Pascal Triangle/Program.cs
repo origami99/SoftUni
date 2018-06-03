06eb8174e75c144862c7489fbdfc177ba56d0f67
origami99 <origamista99@gmail.com>
2018-06-03 23:07:28 +0300

    Solved the "4.Pascal Triangle" task

--- 04. C# Advanced/Exercises/04. Pascal Triangle/Program.cs
+++ 04. C# Advanced/Exercises/04. Pascal Triangle/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        long[][] pascal = new long[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            long[] row = new long[i + 1];
+            row[0] = 1;
+            row[i] = 1;
+
+            for (int j = 1; j < i; j++)
+            {
+                row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+            }
+
+            pascal[i] = row;
+        }
+
+        foreach (long[] row in pascal)
+        {
+            Console.WriteLine(string.Join(" ", row));
+        }
     }
 }
