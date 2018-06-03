2701982b36d00c7b842b2dd826d3e25f4a9b036a
origami99 <origamista99@gmail.com>
2018-06-03 21:23:46 +0300

    Solved the "Sum of Matrix_v3" task

---
+++ 04. C# Advanced/Exercises/01. Sum Matrix Elements_v3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string size = Console.ReadLine();
+        int r = size[0] - '0';
+        int c = size[3] - '0';
+
+        int sum = 0;
+        for (int row = 0; row < r; row++)
+        {
+            sum += Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Sum();
+        }
+
+        Console.WriteLine($"{r}\n{c}\n{sum}");
+    }
+}
