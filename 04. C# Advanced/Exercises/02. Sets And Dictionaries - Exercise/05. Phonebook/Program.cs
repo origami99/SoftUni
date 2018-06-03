3974a340d3c947064f7cdb8388348981de998d9a
origami99 <origamista99@gmail.com>
2018-06-03 10:28:51 +0300

    Solved new task - "Phonebook"

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/05. Phonebook/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/05. Phonebook/Program.cs
@@ -5,6 +5,41 @@ class Program
 {
     static void Main()
     {
+        Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+        while (true)
+        {
+            string[] input = Console.ReadLine().Split('-');
+
+            if (input[0].ToLower() == "search")
+            {
+                break;
+            }
+
+            string name = input[0];
+            string phone = input[1];
+
+            phonebook[name] = phone;
+        }
+
+        while (true)
+        {
+            string name = Console.ReadLine();
+
+            if (name.ToLower() == "stop")
+            {
+                break;
+            }
+
+            if (phonebook.ContainsKey(name))
+            {
+                Console.WriteLine(name + " -> " + phonebook[name]);
+            }
+            else
+            {
+                Console.WriteLine($"Contact {name} does not exist.");
+            }
+        }
 
     }
 }
