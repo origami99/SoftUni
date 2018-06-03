30f1a3241567592ea9b78a4f695265bd2aae69ad
origami99 <origamista99@gmail.com>
2018-06-03 22:23:09 +0300

    Solved the "3. Group Numers" task

--- 04. C# Advanced/Exercises/03. Group Numbers/Program.cs
+++ 04. C# Advanced/Exercises/03. Group Numbers/Program.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        int[] nums = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
+        int[][] matrix = new int[3][];
+
+        for (int i = 0; i <= 2; i++)
+        {
+            matrix[i] = nums.Where(x => Math.Abs(x % 3) == i).ToArray();
+        }
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            Console.WriteLine(string.Join(" ", matrix[i]));
+        }
     }
 }
