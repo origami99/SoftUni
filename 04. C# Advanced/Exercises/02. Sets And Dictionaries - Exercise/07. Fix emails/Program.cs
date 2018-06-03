1e00a3ebf8d9ba099b8f8beda0225c66403356b1
origami99 <origamista99@gmail.com>
2018-06-03 12:41:21 +0300

    Solved 2 new tasks - "Fix emails" and "Hands of cards"

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/07. Fix emails/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/07. Fix emails/Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        Dictionary<string, string> contacts = new Dictionary<string, string>();
 
+        while (true)
+        {
+            string name = Console.ReadLine();
+            if (name == "stop") { break; }
+            string email = Console.ReadLine();
+            
+            contacts[name] = email;
+        }
+
+        contacts = contacts
+            .Where(x => !x.Value.EndsWith("us") && !x.Value.EndsWith("uk"))
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        foreach (KeyValuePair<string, string> res in contacts)
+        {
+            Console.WriteLine(res.Key + " -> " + res.Value);
+        }
     }
 }
--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/08. Hands of cards/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/08. Hands of cards/Program.cs
@@ -1,10 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        Dictionary<string, HashSet<string>> players = new Dictionary<string, HashSet<string>>();
 
+        while (true)
+        {
+            string[] input = Console.ReadLine().Split(':');
+
+            if (input[0] == "JOKER") { break; }
+
+            string name = input[0];
+            HashSet<string> cards = new HashSet<string>(
+                input[1].Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            if (!players.ContainsKey(name))
+            {
+                players.Add(name, cards);
+            }
+            else
+            {
+                players[name].UnionWith(cards);
+            }
+        }
+
+        foreach (KeyValuePair<string, HashSet<string>> player in players)
+        {
+            int points = GetPlayerPoints(player.Value);
+            
+            Console.WriteLine($"{player.Key}: {points}");
+        }
+    }
+
+    private static int GetPlayerPoints(HashSet<string> cards)
+    {
+        int points = 0;
+
+        string[] powers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+        string[] types = { "0", "C", "D", "H", "S" };
+
+        int power = 0;
+        int type = 0;
+
+        foreach (string card in cards)
+        {
+            if (card.Length == 3)
+            {
+                power = 10;
+                type = Array.IndexOf(types, card[2].ToString());
+            }
+            else
+            {
+                power = Array.IndexOf(powers, card[0].ToString());
+                type = Array.IndexOf(types, card[1].ToString());
+            }
+
+            points += power * type;
+        }
+
+        return points;
     }
 }
