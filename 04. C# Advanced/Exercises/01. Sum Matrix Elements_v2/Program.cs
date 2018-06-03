54eb0a31aec6455b2f61dbe3bfc0b064eb1ab16c
origami99 <origamista99@gmail.com>
2018-06-03 21:12:33 +0300

    Solved the "Sum of Matrix_v2" task

--- 04. C# Advanced/Exercises/01. Sum Matrix Elements_v2/Program.cs
+++ 04. C# Advanced/Exercises/01. Sum Matrix Elements_v2/Program.cs
@@ -5,6 +5,25 @@ class Program
 {
     static void Main()
     {
+        string[] size = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+        int r = int.Parse(size[0]);
+        int c = int.Parse(size[1]);
 
+        int[][] matrix = new int[r][];
+        for (int row = 0; row < r; row++)
+        {
+            matrix[row] = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+
+        int sum = 0;
+        foreach (int[] row in matrix)
+        {
+            sum += row.Sum();
+        }
+
+        Console.WriteLine($"{r}\n{c}\n{sum}");
     }
 }
