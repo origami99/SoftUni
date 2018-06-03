bec6544212acaff697b413899940a1361531ad6e
origami99 <origamista99@gmail.com>
2018-06-03 10:37:26 +0300

    Solved new task - "A miner task"

--- 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/06. A miner task/Program.cs
+++ 04. C# Advanced/Exercises/02. Sets And Dictionaries - Exercise/06. A miner task/Program.cs
@@ -5,6 +5,27 @@ class Program
 {
     static void Main()
     {
+        Dictionary<string, int> resourses = new Dictionary<string, int>();
 
+        while (true)
+        {
+            string resourse = Console.ReadLine();
+            if (resourse == "stop") { break; }
+            int quantity = int.Parse(Console.ReadLine());
+
+            if (!resourses.ContainsKey(resourse))
+            {
+                resourses.Add(resourse, quantity);
+            }
+            else
+            {
+                resourses[resourse] += quantity;
+            }
+        }
+
+        foreach (KeyValuePair<string, int> res in resourses)
+        {
+            Console.WriteLine(res.Key + " -> " + res.Value);
+        }
     }
 }
