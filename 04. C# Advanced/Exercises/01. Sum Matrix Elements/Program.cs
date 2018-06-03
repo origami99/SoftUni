e3b4ebbf2eb7393ad7bb4e31388d0783a693f3f6
origami99 <origamista99@gmail.com>
2018-06-03 21:12:01 +0300

    Solved the "Sum of Matrix" task

---
+++ 04. C# Advanced/Exercises/01. Sum Matrix Elements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] size = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+        int r = int.Parse(size[0]);
+        int h = int.Parse(size[1]);
+
+        int[][] matrix = new int[r][];
+        for (int row = 0; row < r; row++)
+        {
+            int[] rowNums = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            matrix[row] = rowNums;
+        }
+
+        int sum = 0;
+
+        for (int row = 0; row < matrix.Length; row++)
+        {
+            for (int col = 0; col < matrix[row].Length; col++)
+            {
+                sum += matrix[row][col];
+            }
+        }
+
+        Console.WriteLine(matrix.Length);
+        Console.WriteLine(matrix[0].Length);
+        Console.WriteLine(sum);
+    }
+}
