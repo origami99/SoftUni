a9b3e2d0a0e60d8162c622b05958821668e79863
origami99 <origamista99@gmail.com>
2018-06-04 11:23:36 +0300

    Added preparation for new tasks and solved "01. Matrix of Palindromes" from "Matrices"

---
+++ 04. C# Advanced/Exercises/03. Multidimentional Arrays - Exercise/01. Matrix of Palindromes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] size = Console.ReadLine().Split();
+        int r = int.Parse(size.First());
+        int c = int.Parse(size.Last());
+
+        string[][] matrix = new string[r][];
+
+        for (int row = 0; row < r; row++)
+        {
+            matrix[row] = new string[c];
+
+            for (int col = 0; col < c; col++)
+            {
+                string elem = string.Format(
+                    $"{(char)(row + 97)}{(char)(col + row + 97)}{(char)(row + 97)}");
+                matrix[row][col] = elem;
+            }
+        }
+
+        foreach (string[] row in matrix)
+        {
+            Console.WriteLine(string.Join(" ", row));
+        }
+    }
+}
