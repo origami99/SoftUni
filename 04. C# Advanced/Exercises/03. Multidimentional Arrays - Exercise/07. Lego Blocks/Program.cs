b5cc6ec7403f15df53dbea1e39c23d2dcc0ad518
origami99 <origamista99@gmail.com>
2018-06-04 22:10:49 +0300

    Solved "07. Lego Blocks" from "Multidimentional Arrays"

--- 04. C# Advanced/Exercises/03. Multidimentional Arrays - Exercise/07. Lego Blocks/Program.cs
+++ 04. C# Advanced/Exercises/03. Multidimentional Arrays - Exercise/07. Lego Blocks/Program.cs
@@ -1,9 +1,46 @@
 ﻿using System;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        string[][] firstBlock = new string[n][];
+        for (int i = 0; i < n; i++)
+        {
+            firstBlock[i] = Console.ReadLine().Split(new char[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        string[][] secondBlock = new string[n][];
+        for (int i = 0; i < n; i++)
+        {
+            secondBlock[i] = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            secondBlock[i] = secondBlock[i].Reverse().ToArray();
+        }
+
+        string[][] matchedBlocks = new string[n][];
+        for (int i = 0; i < n; i++)
+        {
+            matchedBlocks[i] = firstBlock[i].Concat(secondBlock[i]).ToArray();
+        }
+
+        if (matchedBlocks.All(x => matchedBlocks[0].Length == x.Length))
+        {
+            foreach (string[] row in matchedBlocks)
+            {
+                Console.WriteLine("[" + string.Join(", ", row) + "]");
+            }
+        }
+        else
+        {
+            int cells = 0;
+            foreach (string[] row in matchedBlocks)
+            {
+                cells += row.Length;
+            }
+            Console.WriteLine("The total number of cells is: " + cells);
+        }
     }
 }
