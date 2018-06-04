ce5bad44e97b212e7ccc84fa167f266dd54df5db
origami99 <origamista99@gmail.com>
2018-06-04 11:40:05 +0300

    Solved "02. Diagonal Difference" from "Matrices"

--- 04. C# Advanced/Exercises/03. Multidimentional Arrays - Exercise/02. Diagonal Difference/Program.cs
+++ 04. C# Advanced/Exercises/03. Multidimentional Arrays - Exercise/02. Diagonal Difference/Program.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        int[][] matrix = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+
+        int firstDiagSum = 0;
+        int secondDiagSum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            firstDiagSum += matrix[i][i];
+            secondDiagSum += matrix[i][n - i - 1];
+        }
+
+        int diagSumDiff = Math.Abs(firstDiagSum - secondDiagSum);
+
+        Console.WriteLine(diagSumDiff);
     }
 }
