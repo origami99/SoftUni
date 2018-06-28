0c3aee4813a7527898f742dec3539aa1eaea3c6b
origami99 <origamista99@gmail.com>
2018-06-28 22:06:33 +0300

    Solved the "String Matrix Rotation" task from Multidimensional Arrays - Exercise

--- 04. C# Advanced/Exercises/03. Multidimentional Arrays - Exercise/12. String Matrix Rotation/Program.cs
+++ 04. C# Advanced/Exercises/03. Multidimentional Arrays - Exercise/12. String Matrix Rotation/Program.cs
@@ -1,9 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 class Program
 {
     static void Main()
     {
+        Regex regex = new Regex(@"\d+");
+        Match match = regex.Match(Console.ReadLine());
+        int angle = int.Parse(match.Value);
 
+        List<string> inputs = new List<string>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            inputs.Add(input);
+        }
+
+        int r = inputs.Count;
+        int c = inputs.Select(x => x.Length).Max();
+
+        char[][] matrix = new char[r][];
+
+        for (int row = 0; row < r; row++)
+        {
+            int spacesCount = c - inputs[row].Length;
+            string line = inputs[row] + new string(' ', spacesCount);
+            matrix[row] = line.ToArray();
+        }
+
+        matrix = Rotate(matrix, angle);
+
+        foreach (char[] row in matrix)
+        {
+            Console.WriteLine(new string(row));
+        }
+    }
+
+    private static char[][] Rotate(char[][] matrix, int angle)
+    {
+        int timesToTotate = (angle % 360) / 90;
+
+        if (timesToTotate == 0)
+        {
+            return matrix;
+        }
+
+        int oldRowLength = matrix.Length;
+        int oldColLength = matrix.FirstOrDefault().Length;
+
+        char[][] rotatedMatrix = new char[oldColLength][]
+            .Select(x => new char[oldRowLength]).ToArray();
+
+        for (int row = 0; row < rotatedMatrix.Length; row++)
+        {
+            for (int col = 0; col < rotatedMatrix[row].Length; col++)
+            {
+                int oldRow = oldRowLength - col - 1;
+                int oldCol = row;
+                rotatedMatrix[row][col] = matrix[oldRow][oldCol];
+            }
+        }
+
+        return Rotate(rotatedMatrix, angle - 90);
     }
 }
---
+++ 04. C# Advanced/Exercises/03. Multidimentional Arrays - Exercise/12. String Matrix Rotation_v2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Regex regex = new Regex(@"\d+");
+        Match match = regex.Match(Console.ReadLine());
+        int angle = int.Parse(match.Value) % 360;
+
+        List<string> inputs = new List<string>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            inputs.Add(input);
+        }
+
+        int r = inputs.Count;
+        int c = inputs.Select(x => x.Length).Max();
+
+        char[][] matrix = new char[r][];
+
+        for (int row = 0; row < r; row++)
+        {
+            string line = inputs[row] + new string(' ', c - inputs[row].Length);
+            matrix[row] = line.ToArray();
+        }
+
+        switch (angle)
+        {
+            case 90:
+                matrix = Rotate90(matrix);
+                break;
+            case 180:
+                matrix = Rotate180(matrix);
+                break;
+            case 270:
+                matrix = Rotate270(matrix);
+                break;
+        }
+    }
+
+    private static char[][] Rotate90(char[][] matrix)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static char[][] Rotate180(char[][] matrix)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static char[][] Rotate270(char[][] matrix)
+    {
+        throw new NotImplementedException();
+    }
+}
